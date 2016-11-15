5fa5d0e8b675e0b669d12d42d2971f9ef5e08308
Andres Alla <privileeg@users.noreply.github.com>
2016-11-15 14:44:26 +0200

    suured muutused

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <html>
 	<h1>Tere ok</h1>
 		<h4>head aega</h4>
+		h1>Tere ok</h1>
+		<h4>head aega</h4>h1>Tere ok</h1>
+		<h4>head aega</h4>h1>Tere ok</h1>
+		<h4>head aega</h4>h1>Tere ok</h1>
+		<h4>head aega</h4>h1>Tere ok</h1>
+		<h4>head aega</h4>
 </html>
