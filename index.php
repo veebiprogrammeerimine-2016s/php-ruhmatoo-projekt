32e74ab8f0e1c328d49834ecc766653d167c7473
Stanislav Majevski <stasmajevski@users.noreply.github.com>
2016-11-21 21:57:04 +0200

    deleted some files

--- index.php
+++ index.php
@@ -183,7 +183,7 @@ $tyreFittings = getAllTyreFittings();
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 100
         }, 1000);
         return false;
       }
