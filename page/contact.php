5c9022e54dd6fad9a9b90278af6da49b0e62617f
Henri Vajak <henrivajak@users.noreply.github.com>
2016-11-23 11:05:32 +0200

    tegin kontakti ilusamaks

--- page/contact.php
+++ page/contact.php
@@ -1,1 +1,34 @@
-fjseöfjse
+<?php require("../header.php"); ?>
+	
+<div class="container">
+	<body style='background-color:Silver'>
+	    <h1>Töödejuhataja</h1>
+		<label>Henri V.</label>
+	    <br>
+		<label>Telefon 12345678</label>
+	    <br>
+		<label>E-post henriv@koduloomadelaenutus.ee</label>
+		<br>
+					
+			
+		
+	<h1>Koristaja</h1>
+		<label>Tanel M.</label>
+	    <br>
+		<label>Telefon 1122334455</label>
+	    <br>
+	    <label>E-post tanelm@koduloomadelaenutus.ee</label>
+	    <br>
+				
+		
+		
+	<h1>Moraalne tugi</h1>
+		<label>Cleven L.</label>
+	    <br>
+		<label>Telefon 98765432</label>
+	    <br>
+	    <label>E-post clevenl@koduloomadelaenutus.ee</label>
+		<br>		
+	
+</div>
+<?php require("../footer.php"); ?>
