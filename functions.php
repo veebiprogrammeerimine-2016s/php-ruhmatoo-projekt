13a97e9ae5a4229f015861db21975480bb529480
Hinrek <hinrek@gmail.com>
2016-11-23 09:45:10 +0200

    Google login api test #9

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+    require("../config.php");
+
+    /* Session start */
+    session_start();
+        
+    /* Connection */
+    # $database = "if16_hinrek";
+    # $mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+    
+?>
---
+++ parts/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Logi sisse või loo kasutaja</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- Jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Google signin api -->
+<meta name="google-signin-scope" content="profile email">
+<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+</head>
+<body>
