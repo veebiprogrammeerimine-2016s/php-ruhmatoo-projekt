8e37a9916a0e1d15f41e3aa0f9787483243c48f1
Karin Rikkinen <karri1@users.noreply.github.com>
2016-11-23 19:34:20 +0200

    ülesehitus, veel lahtine

--- page/books.php
+++ page/books.php
@@ -5,7 +5,8 @@ require("../class/Book.class.php");
 $Book = new Book($mysqli);                   
  
 //kutsun funktsiooni, et saada kõik raamatud
-$books = $Book->getBooks();    
+$books = $Book->getBooks();
+$tableHtml = "";
 ?>
 
 
@@ -17,38 +18,83 @@ require("../header.php");
 ?>
 
 <h4>Raamatud</h4>
-<?php 
-//kõik raamatud
-$tableHtml = "<br><br>";
-$tableHtml .= "<table>";
-	
-foreach($books as $book){
-	
-	$tableHtml .= "<tr>";
-	
-		if($book->image == ""){
-			$book->image = ("../image/raamat.jpg");  //kui raamatust pilti pole
-		}
-		$tableHtml .= "<td>";  //link detailse vaateni
-			$tableHtml .= '<a href="details.php?id='.$book->book_id.'"> 
-								<img src="' .$book->image. '" style= "width:128px;" >
-							</a>';
-		$tableHtml .= "</td>";
-		
-		$tableHtml .= "<td>";
-			$tableHtml .= "<div>" .$book->author."</div>";
-			$tableHtml .= '<p><a href="details.php?id='.$book->book_id.'">' .$book->title.'</a></p>';	//link detailse vaateni
-		$tableHtml .= "</td>";
-	
-	$tableHtml .= "</tr>";
-	
-}
-$tableHtml .= "</table>";	
-echo $tableHtml;
-	
-	
-	
+<table>
+<tbody>
 
-?>
-
+<!--1. RIDA....OTSING JA SORTEERIMINE-->
+	<tr>  
+	   <p>otsing ja sorteerimine</p>                               <!--???-->
+	</tr>
+	
+<!--2. RIDA....KATEGOORIAD JA RAAMATUD-->
+	<tr>
+	    <!--lahter 1 kategooriad -->
+		<td>
+		   
+			<div id="topic">
+				<a href="books.php?topic=kategooria1" alt="kategooria 1" title="kategooria 1">kategooria 1</a>       <!--???-->
+			</div>
+			<div id="topic">
+				<a href="books.php?topic=kategooria2" alt="kategooria 2" title="kategooria 2">kategooria 2</a>
+			</div>
+			<div id="topic">
+				<a href="books.php?topic=kategooria3" alt="kategooria 3" title="kategooria 3">kategooria 3</a>
+			</div>
+			<!--JNE-->
+													
+		</td>
+		<!--lahter 2 leheküljed ja raamatud -->
+		<td>
+	       <!--Näitab kus kategoorias, algul kõik -->
+			 <div id="tree">
+				<p><a href="/books.php">Valik</a>&gt; Kõik raamatud </p>  <!--??? -->
+			</div>
+			<!--leheküljed  1-20 21-40 jne ÜLEVAL -->
+			<div id="numeric">1-20 | 
+				<a href="books.php?page=20">21-40</a> |                       <!--??? -->
+				<a href="books.php?page=40">41-60</a> | 
+				<a href="books.php?topic=kategooria1&page=60">61-80</a> |     <!--näiteks ??? -->
+				<a href="books.php?topic=kategooria2&page=80">81-100</a> |  
+			</div>
+			<!--kõik raamatud-->
+			<div>
+			
+			<?php 				
+				$tableHtml .= "<table>";
+					
+				foreach($books as $book){
+					
+					$tableHtml .= "<tr>";
+					
+						if($book->image == ""){
+							$book->image = ("../image/raamat.jpg");  //kui raamatust pilti pole
+						}
+						$tableHtml .= "<td>";  //link detailse vaateni
+							$tableHtml .= '<a href="details.php?id='.$book->book_id.'"> 
+												<img src="' .$book->image. '" style= "width:128px;" >
+											</a>';
+						$tableHtml .= "</td>";
+						
+						$tableHtml .= "<td>";
+							$tableHtml .= "<div>" .$book->author."</div>";
+							$tableHtml .= '<p><a href="details.php?id='.$book->book_id.'">' .$book->title.'</a></p>';	//link detailse vaateni
+						$tableHtml .= "</td>";
+					
+					$tableHtml .= "</tr>";
+					
+				}
+				$tableHtml .= "</table>";	
+				echo $tableHtml;									
+			?>
+			
+			</div>
+			<!--leheküljed  1-20 21-40 jne ALL -->
+			<div id="numeric">1-20 | 
+				<a href="books.php?page=20">21-40</a> | 
+				<a href="books.php?page=40">41-60</a> | 	
+			</div>
+		</td>
+	</tr>
+</tbody>
+</table>
 <?php require("../footer.php");?>
