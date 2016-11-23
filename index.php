2d545752494a425e63422c8bc546d40327e9d012
Hinrek <hinrek@gmail.com>
2016-11-23 10:47:33 +0200

    Redirect index.php

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
 
-// require "/functions.php";
+header("Location: /page/login.php");
 
 ?>
-<?php require "/var/www/kool/php-ruhmatoo-projekt/parts/header.php";?>
-<body>
-    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-    <script>
-      function onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
-
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
-      };
-    </script>
-</body>
-<?php require "/var/www/kool/php-ruhmatoo-projekt/parts/footer.php";?>
