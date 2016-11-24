48f7ca1aa89ee2a5532b6e848359438076cd72cb
Mihkel Põder <mpoder@users.noreply.github.com>
2016-11-24 11:19:34 +0200

    fixed comment

--- page/header.php
+++ page/header.php
@@ -2,7 +2,7 @@
 ?>
 <html>
 <head>
-//<link rel="stylesheet" type="text/css" href="../styles/default.css">
+/*<link rel="stylesheet" type="text/css" href="../styles/default.css">*/
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
 <script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
