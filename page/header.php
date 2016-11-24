aeacc57bf88ccab9e5a022392bc0480a0718dead
Mihkel Põder <mpoder@users.noreply.github.com>
2016-11-24 12:52:04 +0200

    quick fix for line height

--- page/header.php
+++ page/header.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
 <script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+<link rel="stylesheet" href="../styles/default.css">
 </head>
 <body>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
