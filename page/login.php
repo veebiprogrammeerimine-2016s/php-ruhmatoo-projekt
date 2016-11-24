b3533d68234142abcee1900de0ecdcf8bf9b5175
Kristel Roosimaa <krisr12@users.noreply.github.com>
2016-11-24 12:10:30 +0200

    email, password

--- page/login.php
+++ page/login.php
@@ -9,8 +9,24 @@ require("header.php");
 
 <h2>Logi Sisse</h2>
 
+<!-- Simple Textfield -->
+<form action="#">
+  <div class="mdl-textfield mdl-js-textfield">
+    <input class="mdl-textfield__input" type="text" id="sample1">
+    <label class="mdl-textfield__label" for="sample1">E-mail</label>
+  </div>
+</form>
+
+<!-- Simple Textfield -->
+<form action="#">
+  <div class="mdl-textfield mdl-js-textfield">
+    <input class="mdl-textfield__input" type="text" id="sample2">
+    <label class="mdl-textfield__label" for="sample1">Parool</label>
+  </div>
+</form>
 
 
+		
 
 <?php
 require("footer.php");
