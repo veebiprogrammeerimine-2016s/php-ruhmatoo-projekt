f1a5ab8c589834646d37c3e8841de3eb93fbe188
Richard Kajaste <rkajaste@users.noreply.github.com>
2016-11-24 13:04:13 +0200

    Added searchbox and CSS

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+
+<title>NoTime2Spare</title>
+
+<head>
+<h1>Home</h1>
+<?php require('styles.css'); ?>
+</head>
+
+<body>
---
+++ page/search.php
@@ -0,0 +1,7 @@
+<?php
+require('../header.php');
+?>
+<form>
+	<input type="text" name="search" placeholder="Enter title, keywords etc...">	
+</form>
+<?php require('../footer.php'); ?>
