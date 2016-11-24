e82f5caa631aa7ff9f7d410160911ede77d1865d
Stenly Poks <stenlypoks@users.noreply.github.com>
2016-11-24 15:10:31 +0200

    vale parool, email jääb

--- page/login.php
+++ page/login.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 
 	require("../functions.php");
-	
+
     require("../class/Helper.class.php");
 	$Helper = new Helper();
-	
+
 	require("../class/User.class.php");
 	$User = new User($mysqli);
-	
+
 	//kui on sisse loginud siis suunan data lehele
 	if(isset($_SESSION["userId"])){
 		header("Location: data.php");
 		exit();
 	}
-	
+
 	//kui tahab kasutajat luua siis suuna signup lehele
 	/*if(isset(                            )){
 		header("Location: signup.php");
 		exit();
 	}
 	*/
-	
-	
+
+
 	//var_dump($_GET);
 	//echo "<br>";
 	//var_dump($_POST);
@@ -29,29 +29,20 @@
 	// MUUTUJAD
 	$loginEmail = "";
 
-	
-	if(isset ($_POST["loginEmail"])) {
-		if(empty ($_POST["loginEmail"])){
-			$signupEmailError = "* Väli on kohustuslik!";
-		} else{
-		$loginEmail = $_POST["loginEmail"];
-		}
-	}	
-	
-	
-	
 	$notice ="";
 	//kas kasutaja tahab sisse logida
 	if( isset($_POST["loginEmail"]) &&
 		isset($_POST["loginPassword"]) &&
 		!empty($_POST["loginEmail"]) &&
-		!empty($_POST["loginPassword"]) 
+		!empty($_POST["loginPassword"])
 	){
 		$notice = $User->login($_POST["loginEmail"], $_POST["loginPassword"]);
 	}
-	
-	
+
+
 ?>
+
+
 
 
 <?php require("../loginheader.php");?>
@@ -63,14 +54,12 @@
 		<form method="POST" >
 			<label>E-post</label><br>
 			<div class="form-group">
-					<input class="form-control" name="loginEmail" type="email">
+					<input class="form-control" name="loginEmail" type="email" value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } ?> class="textbox required email" />
 			</div>
-
 			<label>Parool</label><br>
 			<div class="form-group">
 			<input class="form-control" name="Parool" type="password">
 			</div>
-
 			<br><br>
 			<input class="btn btn-success btn-sm hidden-xs" type="submit" value="Logi sisse">
 			<input class="btn btn-success btn-sm btn-block visible-xs-block" type="submit" value="Logi sisse2">
