42ec6cfbc167529d626ee0bd6b661e63858a5534
taankomm <taankomm@greeny.cs.tlu.ee>
2016-11-24 14:26:27 +0200

    Added header/footer. started with data.php

--- page/data.php
+++ page/data.php
@@ -1,3 +1,26 @@
 <?php
 
+require("../functions.php");
+
+require("../class/Helper.class.php");
+$Helper = new Helper();
+
+require("../class/Event.class.php");
+$Event = new Event($mysqli);
+
+//kui ei ole sisseloginud, suunan login lehele
+if (!isset($_SESSION["userId"])) {
+  header("Location: login.php");
+  exit();
+}
+
+//kas aadressireal on logout
+if (isset($_GET["logout"])) {
+
+  session_destroy();
+
+  header("Location: login.php");
+  exit();
+
+}
 ?>
