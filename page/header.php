9fe8acdb9c87b107f0fcfc14949402822bb76f42
Mihkel Põder <mpoder@users.noreply.github.com>
2016-11-25 08:52:59 +0200

    fixed a small typo

--- page/header.php
+++ page/header.php
@@ -2,13 +2,13 @@
 	require("../function/functions.php");
 ?>
 <html>
-<head>
+<header>
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
 <script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
 <link rel="stylesheet" href="../styles/default.css">
 
-</head>
+</header>
 <body>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
