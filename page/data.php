b9c5bf9605f1eef73c91bfad56b12a87026a4ac6
Joosep Juhanson <Juhanson@users.noreply.github.com>
2016-11-29 22:28:49 +0200

    started work on registering rides

--- page/data.php
+++ page/data.php
@@ -23,13 +23,71 @@ if (isset($_GET["logout"])) {
   exit();
 
 }
+
+if ( isset($_POST["start_location"]) &&
+isset($_POST["start_time"]) &&
+isset($_POST["arrival_location"]) &&
+isset($_POST["arrival_time"]) &&
+isset($_POST["free_seats"]) &&
+!empty($_POST["age"]) &&
+!empty($_POST["start_location"]) &&
+!empty($_POST["start_time"]) &&
+!empty($_POST["arrival_location"]) &&
+!empty($_POST["arrival_time"]) &&
+!empty($_POST["free_seats"]))
+
+/*
+ {
+    $color = $Helper->cleanInput($_POST["start_location"]);
+    $Rides->saveEvent($Helper->cleanInput($_POST["price"]), $start_location);
+}
+
+{
+    echo "Saved.";
+    saveEvent(($_SESSION["userEmail"]),($_POST["datestamp"]),($_POST["exercise"]), ($_POST["sets"]), ($_POST["reps"]), ($_POST["weight"]));
+}
+*/
+
+
 ?>
 
 <h1>Data</h1>
 
 <p>
 
-    Tere tulemast <?=$_SESSION["userEmail"];?>!
-    <a href="?logout=1">logi välja</a>
+    Welcome <?=$_SESSION["userEmail"];?>!
+    <a href="?logout=1">log out</a>
 
 </p>
+
+
+<h2>Register a ride</h2>
+<form method="POST" >
+
+    <label>Start location</label><br>
+    <input name="start_location" type="text">
+
+    <br><br>
+    <label>Start time</label><br>
+    <input name="start_time" type="text">
+
+    <br><br>
+    <label>Arrival location</label><br>
+    <input name="arrival_location" type="text">
+
+    <br><br>
+    <label>Arrival time</label><br>
+    <input name="arrival_time" type="text">
+
+    <br><br>
+    <label>Free seats</label><br>
+    <input name="free_seats" type="number">
+
+    <br><br>
+    <label>Price</label><br>
+    <input name="price" type="number">
+
+    <br><br>
+    <input type="submit" value="Submit">
+
+</form>
