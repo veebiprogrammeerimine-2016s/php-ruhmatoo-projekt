08c6c595b43cf5d35dac0ca8e59c5e94dd4ffa88
Mihkel Põder <mpoder@users.noreply.github.com>
2016-11-30 20:54:19 +0200

    Some styling for the profile page

--- page/profile_user.php
+++ page/profile_user.php
@@ -1,38 +1,23 @@
 <?php
 require("header.php");
 ?>
-
+<style>
+.left {
+	float: left;
+	max-width: 20%;
+}
+.right {
+	float: right;
+	max-width: 80%;
+}
+</style>
 <title>Kasutaja && profiil</title>
-
-<!-- Always shows a header, even in smaller screens. -->
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-  <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <!-- Title -->
-      <span class="mdl-layout-title"><?php echo $user; ?> profiil</span>
-      <!-- Add spacer, to align navigation to the right -->
-      <div class="mdl-layout-spacer"></div>
-      <!-- Navigation. We hide it in small screens. -->
-      <nav class="mdl-navigation mdl-layout--large-screen-only">
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
-  </header>
-  <div class="mdl-layout__drawer">
-    <span class="mdl-layout-title"><?php echo $user; ?> profiil</span>
-    <nav class="mdl-navigation">
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-    </nav>
-  </div>
-  <main class="mdl-layout__content">
-    <div class="page-content"><!-- Your content goes here --></div>
-  </main>
+<div class="left">
+<h2>Kasutaja</h2>
+<p><b>Nimi:</b> <?php echo "NULL";?></p>
 </div>
+<div class="right">
+</div>
+
 
 <?php require("footer.php"); ?>
