c5120a5db8100a3246a6dbf3646553dcc2e93319
Stenly Poks <stenlypoks@users.noreply.github.com>
2016-11-30 15:18:26 +0200

    Andmebaasi vahetus

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 	require("/home/railtoom/config.php");
-	
+
 	//see fail peab olema seotud kõigiga kus tahame sessiooni kasutada, saab kasutada nüüd $_session muutujat
 	session_start();
-	$database = "if16_raily_4";
+	$database = "if16_raily_4_projekt";
 	///ainus koht kust andmebaasi ühendus
 	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
-	
+
 ?>
