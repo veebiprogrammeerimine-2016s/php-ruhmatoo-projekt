cf4543edb31ec5fd351fe1d4de68d6c6e2457650
Kert Aavik <kertaavik@users.noreply.github.com>
2016-12-01 12:02:40 +0200

    Lisasin avalehele E-mail formi

--- js/contact_me.js
+++ js/contact_me.js
@@ -30,14 +30,15 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
+                success: function(e) {
+					console.log(e);
                     // Enable button & show success message
                     $("#btnSubmit").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Sinu sõnum on edastatud </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,7 +50,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Vabandame " + firstName + ", Palun proovige uuesti");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
