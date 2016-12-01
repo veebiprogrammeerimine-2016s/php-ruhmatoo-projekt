0cbe80e763373b14fce577a835a2592a34600477
Mariam Reintop <MReintop@users.noreply.github.com>
2016-12-01 16:54:37 +0200

    otsing plants lehel

--- page/care.php
+++ page/care.php
@@ -12,138 +12,33 @@ if(isset($_SESSION["userID"])){
 
 	
 	//MUUTUJAD
-	$signupEmailError = "";
-	$signupPasswordError = "";
-	$registerEmailError = "";
-	$registerPasswordError ="";
-	$signupEmail = "";	
-	$registerEmail = "";
-	
-	$nameError = "";
-	$userFirstName= "";
-	$userLastName="";
 	
 	
-	
-
-	
-	if(isset($_POST["FirstName"])){
+	if(isset($_GET["q"])){
+		$q = $Helper->cleanInput($_GET["q"]);
+		$plantData = $Plant->getAll($q, $sort, $direction);
+	} else {
+		$q="";
+		$plantData = $Plant->getAll($q,$sort,$direction);
 		
-		if(empty($_POST["FirstName"])){
-			
-			$nameError = "Ees-ja perekonnanimi on kohustuslikud!";
-		} else {
-			
-			$userFirstName=$_POST["FirstName"];
-			
-		}
+	}	
+		
+		if(isset($_GET["sort"]) && isset($_GET["direction"])){
+		$sort=$_GET["sort"];
+		$direction=$_GET["direction"];
+	} else {
+		//kui ei ole määratud siis vaikimisi id ja ASC
+		$sort="id";
+		$direction="ascending";
+		
 	}
 	
-	if(isset($_POST["LastName"])){
+	if (isset($_GET["logout"])) {
 		
-		if(empty($_POST["LastName"])){
-			
-			$nameError = "Ees-ja perekonnanimi on kohustuslikud!";
-		} else {
-			$userLastName=$_POST["LastName"];
-			
-		}
-	}
-
-	if( isset($_POST["signupEmail"] )){
-
-	
-
-		if( empty($_POST["signupEmail"])) {
-
-			$signupEmailError = "see väli on kohustuslik";
-			
-		} else {
-			
-			//email olemas
-			$signupEmail=$_POST["signupEmail"];
-
-
-
-			}
-	}
-
-
-
-	if( isset($_POST["signupPassword"])) {
-
-		if( empty($_POST["signupPassword"])) {
-
-			$signupPasswordError = "see väli on kohustuslik";
-		}else{
-		//Siia jõuan siis, kui parool oli olemas ja parool ei olnud tühi. !ELSE!
-			if(strlen($_POST["signupPassword"])<8) {
-
-				$signupPasswordError = "Parool peab olema vähemalt 8 märki pikk";
-			}
-
-
-
-
-	}
-	}
-
-	if( isset($_POST["registerEmail"] )){
-
+		session_destroy();
+		header("Location: login.php");
+		exit();
 		
-
-		if( empty($_POST["registerEmail"])) {
-
-			$registerEmailError = "e-mail on kohustuslik";
-         }else{
-			
-			//email olemas
-			$registerEmail=$_POST["registerEmail"];
-
-
-
-
-		}
-	}
-
-
-
-if( isset($_POST["registerPassword"] )){
-
-		
-
-		if( empty($_POST["registerPassword"])) {
-
-			$registerPasswordError = "parool on kohustuslik";
-			
-			
-		} else {
-
-             if(strlen($_POST["registerPassword"])<8) {
-
-				$registerPasswordError = "Parool peab olema vähemalt 8 märki pikk";
-			}
-
-		}
-	}
-
-
-	
-	
-	if($registerEmailError == "" && empty ($registerPasswordError) && isset($_POST["registerEmail"])
-			&& isset($_POST["registerPassword"])&&  isset($_POST["FirstName"]) 
-			&& isset($_POST["LastName"])) {
-		
-		
-		
-		$password = hash("whirlpool", $_POST["registerPassword"]);
-		
-		
-		$User->signUp(($Helper->cleanInput($registerEmail)),($Helper->cleanInput($password)),($Helper->cleanInput($userFirstName)),($Helper->cleanInput($userLastName)));
-	
-		echo "Salvestan...";
-		echo "email : ".$_POST["registerEmail"]."<br>";
-	
 	}
 	
 //var_dump($_POST);
@@ -170,8 +65,53 @@ if( isset($_POST["signupEmail"]) && isset($_POST["signupPassword"])&&
 	<div class="tab-content">
 		<div role="tabpanel" class="tab-pane active" id="plantsTab">
 			
-			<div id="plantCare" class="container">
+			<div id="plantCare" class="row col-md-8">
 			  <p>Siia tulevad Pländid!!</p>
+			  <?php
+						
+						$direction = "ascending";
+						if (isset($_GET["direction"])){
+							if($_GET["direction"]=="ascending"){
+								$direction="descending";
+							}
+							
+						}
+						
+						$html = "<table class='table table-striped table-hover table-condensed table-bordered  ' style='background-color:white'>";
+						$html .= "<tr>";
+							$html .= "<th><a href='?q=".$q."&sort=id&direction=".$direction."'>nr</a></th>";
+							$html .= "<th><a href='?q=".$q."&sort=id&direction=".$direction."'>id</a></th>";
+							$html .= "<th><a href='?q=".$q."&sort=plant&direction=".$direction."'>plant</a></th>";
+							$html .= "<th><a href='?q=".$q."&sort=interval&direction=".$direction."'>interval</a></th>";
+						$html .= "</tr>";
+						
+						$i = 1;
+						//iga liikme kohta massiivis
+						foreach($plantData as $p) {
+							//iga taim on $p
+							//echo $p->taim."<br>";
+						
+							
+							$html .= "<tr>";
+								$html .= "<td>".$i."</td>";
+								$html .= "<td>".$p->id."</td>";
+								$html .= "<td>".$p->taim."</td>";
+								$html .= "<td>".$p->intervall."</td>";
+								$html .= "<td><a href='edit.php?id=".$p->id."'>muuda</a></td>";
+							$html .= "</tr>";
+							
+							$i += 1;
+						}
+						
+						$html .= "</table>";
+						
+						echo $html;
+						
+						$listHtml="<br><br>";
+						
+						
+						
+						echo $listHtml;?>
 			</div>
 
 					
@@ -179,6 +119,11 @@ if( isset($_POST["signupEmail"]) && isset($_POST["signupPassword"])&&
         <div role="tabpanel" class="tab-pane" id="Otsing">
 			
 			<p>Siia tuleb otsing!</p>
+			<h3>Taime otsing</h3>
+										<form>
+												<input type="search" name="q" value="<?=$q;?>">
+												<input class="btn btn-default" type="submit" value="Otsi">
+										</form>
 		</div>	
 	</div>	
 </div>
