e776900faa8b34213bf7d0b9cd0777a1811a4d7b
Greg <gregnesselmann@gmail.com>
2016-12-01 14:51:12 +0200

    username

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
 
-		<!-- Latest compiled and minified JavaScript -->
+		<!-- Latest compiled and minified JavaScript tra git -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		
 		
