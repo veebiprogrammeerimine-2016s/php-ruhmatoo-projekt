a33aa90a4ec5cc6b0ecbdcf7d768b4722205426c
Richard Kajaste <rkajaste@users.noreply.github.com>
2016-12-01 16:01:14 +0200

    Parsed directors, rating and synopsis

--- josn.php
+++ josn.php
@@ -8,35 +8,52 @@ $b = 0;
 echo "<ol>";
 foreach($movies as $m){
 	
-	if($b == 2){ break; }
+	if($b == 100){ break; }
 	
 	echo "<li>".$m->title."</li>";
-	echo "<a href='https://www.rottentomatoes.com".$m->url."'>link</a>";
+	echo "<a href='https://www.rottentomatoes.com".$m->url."'>link</a><br>";
 	$html = file_get_html("https://www.rottentomatoes.com".$m->url);
-	var_dump($html->find('div[class=info]')[0]->find('div[class=col]')[1]->plaintext);
+	echo $html->find('div[class=info]')[0]->find('div[class=col]')[1]->plaintext."<br>";
+	echo $html->find('div[class=info]')[0]->find('div[class=col]')[3]->plaintext."<br>";
+	$directors = $html->find('div[class=info]')[0]->find('div')[5]->find("a");
+	$i=0;
+	echo "Directors: ";
+	foreach($directors as $d){
+		
+		if($i < (count($directors)-1) && count($directors) > 1){
+			
+			echo $d->plaintext.", ";
+			
+		}else{
+			
+			echo $d->plaintext;
+			
+		}
+		$i+=1;
+	}
 	echo "<br>";
-	var_dump($html->find('div[class=info]')[0]->find('div[class=col]')[3]->plaintext);
-	echo "<br>";
-	var_dump($html->find('div[class=info]')[0]->find('div')[5]->find("a")[0]->plaintext);
-	echo "<br>";
-	var_dump($html->find('div[class=info]')[0]->find('div[class=col]')[8]->plaintext);
-	echo "<br>";
-	
-	echo $m->url."<br>";
+	echo $html->find('div[class=info]')[0]->find('div[class=col]')[8]->plaintext."<br>";	
 	foreach($m->posters as $p){
 		echo $p."<br>";
 	}
 	$i = 0;
+	echo "Actors: ";
 	foreach($m->actors as $a){
-		if($i <= count($a)){
+		
+		if($i < (count($m->actors)-1) && count($m->actors) > 1){
+			
 			echo $a.", ";
+			
 		}else{
+			
 			echo $a;
+			
 		}
 		$i+=1;
 	}
-	echo "<br>".$m->runtime;
-	
+	echo "<br>".$m->runtime."<br>";
+	echo "Tomato Score: ".$m->tomatoScore."<br>";
+	echo "Synopsis: ".$m->synopsis;
 	$b++;
 }
 echo "</ol>";
