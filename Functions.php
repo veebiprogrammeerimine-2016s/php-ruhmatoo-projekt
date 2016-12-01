1629cc7ef3dde16f62a4089d79c8384da2821121
Kristjan Liiva <Zargorath@users.noreply.github.com>
2016-12-01 11:33:43 +0200

    Alustasin funktsioonide lisamist

---
+++ Functions.php
@@ -0,0 +1,49 @@
+<?php
+
+	require("../../config.php")
+	
+	$database = "if16_kliiva"
+
+	
+	
+	function SaveClientData($Name, $Telephone, $Email, $Add_info) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("INSERT INTO client_data (Name, Telephone, Email, Additional_info) VALUES (?, ?, ?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("siss", $Name, $Telephone, $Email, $Add_info);
+		if ($stmt->execute()) {
+			
+			echo "Salvestamine õnnestus";
+			
+		} else {
+			
+			echo "Ilmnes viga".$stmt->error;
+			
+		}
+		
+	}
+	
+	function SaveVeichleData($reg_nr, $veichle_types, $) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("INSERT INTO veichle_data (Registration_nr, Veichle_type, Veichle_brand, Veichle_model)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ssss", $reg_nr, $veichle_types, $car_brand, $car_model);
+		if ($stmt->execute()) {
+			
+			echo "Salvestamine õnnestus";
+			
+			
+		} else {
+			
+			echo "Ilmnes viga".$stmt->error;
+			
+		}
+	}
+
+?>
--- Reservations.php
+++ Reservations.php
@@ -2,6 +2,55 @@
 
 	//require("functions.php")
 	require("Header.php")
+	
+	$reg_nr = "";
+	$car_brand = "";
+	$car_model = "";
+	$reg_nr_Error = "";
+	$car_model_Error = "";
+	$car_brand_Error = "";
+	
+	if (isset ($_POST["reg_nr"])) {
+		
+		if (empty($_POST["reg_nr"])) {
+			
+			$reg_nr_Error = "Palun sisestage registreerimisnumber";
+			
+		} else {
+			
+			$reg_nr = $_POST["reg_nr"];
+			
+		}
+		
+	}
+	
+	if (isset ($_POST["car_brand"])) {
+		
+		if (empty($_POST["car_brand"])) {
+			
+			$car_brand_Error = "Palun sisestage enda automark";
+			
+		} else {
+			
+			$car_brand = $_POST["car_brand"];
+			
+		}
+		
+	}
+	
+	if (isset($_POST["car_model"])) {
+		
+		if (empty($_POST["car_model"])) {
+			
+			$car_model_Error = "Palun sisestage enda automudel";
+			
+		} else {
+			
+			$car_model = $_POST["car_model"];
+			
+		}
+		
+	}
 ?>
 
 <!DOCTYPE html>
@@ -11,35 +60,33 @@
 	</head>
 	<body>
 	<div class="container">
-		<form>
+		<form method="post">
 			<div class="form-group row">
 			<div class="col-sm-10">
 			<h1>Sõiduki Andmed</h1><br>
 			</div>
-		
 				<label for="reg_nr" class="col-sm-4 col-form-label">Registreerimisnumber</label>
 				<div class="col-sm-10">
-					<input type="text" class="form-control" id="reg_nr">
+					<input type="text" class="form-control" name="reg_nr">
 				</div>
 			</div>
-			
 		<fieldset class="form-group row">
-        <legend class="col-form-legend col-sm-4">Sõiduki tüüp</legend>
-        <div class="col-sm-10">
-        <div class="form-check">
-          <label class="form-check-label">
+			<legend class="col-form-legend col-sm-4">Sõiduki tüüp</legend>
+				<div class="col-sm-10">
+					<div class="form-check">
+				<label class="form-check-label">
             <input class="form-check-input" type="radio" name="veichle_types" id="veichle_type1" value="Sõiduauto" checked>
             Sõiduauto
           </label>
         </div>
-        <div class="form-check">
-          <label class="form-check-label">
+			<div class="form-check">
+				<label class="form-check-label">
             <input class="form-check-input" type="radio" name="veichle_types" id="veichle_type2" value="Maastur">
             Maastur
           </label>
         </div>
-        <div class="form-check">
-          <label class="form-check-label">
+			<div class="form-check">
+				<label class="form-check-label">
             <input class="form-check-input" type="radio" name="veichle_types" id="veichle_type3" value="Kaubik">
             Kaubik
           </label>
@@ -48,10 +95,10 @@
 				
 			<div class="form-group row">
 				<label for="car_brand" class="col-sm-4 col-form-label">Mark</label>
-				<div class="col-sm-10">
-					<input type="text" class="form-control" id="car_brand">
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="car_brand">
+					</div>
 				</div>
-			</div>
 				
 				<div class="form-group row">
 				<label for="car_model" class="col-sm-4 col-form-label">Mudel</label>
@@ -59,8 +106,7 @@
 					<input type="text" class="form-control" id="car_model">
 				</div>
 			</div>
-			
-				<fieldset class="">
+		
 
 		</form>
 	</body>
