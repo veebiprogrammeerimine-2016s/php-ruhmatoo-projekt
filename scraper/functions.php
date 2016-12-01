91e5ae4c2fa1341b15fb31642c78e91f21616b51
Alar Aasa <alaraasa@users.noreply.github.com>
2016-12-01 13:17:26 +0200

    #3 Getting dates with Simple HTML DOM

---
+++ scraper/functions.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Alar Aasa <alar@alaraasa.ee>
+ */
+
+function monthToDate($month){
+    if ($month == "jaanuar" || $month == "Jaanuar"){
+        return"01";
+    }
+    if ($month == "veebruar" || $month == "Veebruar"){
+        return"02";
+    }
+    if ($month == "märts" || $month == "Märts"){
+        return"03";
+    }
+    if ($month == "aprill" || $month == "Aprill"){
+        return"04";
+    }
+    if ($month == "mai" || $month == "Mai"){
+        return"05";
+    }
+    if ($month == "juuni" || $month == "Juuni"){
+        return"06";
+    }
+    if ($month == "juuli" || $month == "Juuli"){
+        return"07";
+    }
+    if ($month == "august" || $month == "August"){
+        return"08";
+    }
+    if ($month == "september" || $month == "September"){
+        return"09";
+    }
+    if ($month == "oktoober" || $month == "Oktoober"){
+        return"10";
+    }
+    if ($month == "november"){
+        return"11";
+    }
+    if ($month == "detsember"){
+        return"12";
+    }
+}
--- scraper/scraper.php
+++ scraper/scraper.php
@@ -1,5 +1,6 @@
 <?php
-mb_internal_encoding("ISO-8859-13");
+include "simple_html_dom.php";
+require_once ("functions.php");
 // Specialization
 if (isset($_GET["ryhm"])) {
     $ryhm = $_GET["ryhm"];
@@ -11,7 +12,7 @@ if (isset($_GET["ryhm"])) {
 if (isset($_GET["time"])) {
     $time = $_GET["time"];
 } else {
-    $time = 1480703647;
+    $time = time();
 }
 
 // Group if exists for filtering results
@@ -20,44 +21,27 @@ if (isset($_GET["grupp"])) {
 } else {
     $grupp = 1;
 }
-
+$data = [];
 $url = 'http://www.tlu.ee/masio/index.php?id=ryhm&ryhm=' . $ryhm . '&time=' . $time . '#MASIO';
+$html = file_get_html($url);
+// GET DATES
+foreach ($html->find('div.dayname') as $dates){
+    //remove HTML tags
+    $dates = substr(strstr($dates, " "), 17);
+    $dates = substr($dates,0 , -6);
 
-function curl($url)
-{
-    $options = Array(
-        CURLOPT_RETURNTRANSFER => TRUE, //Return website data
-        CURLOPT_FOLLOWLOCATION => TRUE, //Follow "location" headers
-        CURLOPT_AUTOREFERER => TRUE, // Set referrer as the "location" header address
-        CURLOPT_CONNECTTIMEOUT => 120, // Seconds in which the request times out
-        CURLOPT_TIMEOUT => 30, // Maximum amount of time per query
-        CURLOPT_MAXREDIRS => 10, // Maximum amount of redirects
-        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0 Waterfox/50.0",
-        CURLOPT_URL => $url //Where to scrape from
-    );
-    $ch = curl_init();
-    curl_setopt_array($ch, $options);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
+    $datesExplode = explode(' ', $dates);
+    array_shift($datesExplode);
+    // Rename full date name to number
+    $datesExplode[1] = monthToDate($datesExplode[1]);
+    $dates = implode("", $datesExplode);
+    array_push($data, $dates);
 }
 
-function scrape_between($data, $start, $end)
-{
-    $data = stristr($data, $start);
-    $data = substr($data, strlen($start));
-    $stop = stripos($data, $end);
-    $data = substr($data, 0, $stop);
-    return $data;
-}
+var_dump($data);
 
-/*TODO
- * Get current date from MASIO "Täna" link.
- *
- */
-$scraped_page = curl($url);
-$scraped_data = scrape_between($scraped_page, "<div class=\"dayname\">", "<table style=\"border-top:solid 3px #b50001;width:100%;\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">");
-echo $scraped_data;
+
+
 
 
 
