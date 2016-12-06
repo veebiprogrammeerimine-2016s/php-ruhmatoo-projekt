64362fbb40551aa07004ece90af8d997acc8ce1f
Ingo Mägi <ingomagi@users.noreply.github.com>
2016-12-06 14:32:48 +0200

    added bootstrap element

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Logi sisse või loo kasutaja</title>
+	<title>Veini Pood</title>
 	<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
