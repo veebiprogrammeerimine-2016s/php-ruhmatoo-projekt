4514fc597b33ddb95880f1bd97d8adf981016d50
Xenia <XeniaBelor@users.noreply.github.com>
2016-12-08 00:45:37 +0200

    Natukene redakteerisin kakategooriad ja komments

--- homepage.php
+++ homepage.php
@@ -1,4 +1,4 @@
-<?php
+Ôªø<?php
 	
 	require("functions.php");
 	
@@ -30,26 +30,55 @@
 
 <html>
 Tere tulemast <?=$_SESSION["userEmail"];?>!
-<a href="?logout=1">Logi v‰lja</a>
+<a href="?logout=1">Logi v√§lja</a>
+
+<style>
+	
+	.cat_col{
+		color:#A11F2F;
+		font-weight: 600;
+	}
+	
+</style>
+
 <body>
 	
 	<form method="POST">
 	<!--KATEGOORIA-->
-	<p><label for="category">Valikategooria:</label><br>
-	<select name="category" id="category">
-	<option value="">kategoria 1</option>
-	<option value="kategooria1">kategooria1.1</option>
-	<option value="kategooria1.2">kategooria1.2</option>
-	<option value="kategooria2.2">kategooria2.1</option>
-	<option value="kategooria2.2">kategooria2.2</option>
-	</select>
-	<!--pealkiri-->
-	<label for="headline">headline</label><br>
-	<input name="headline">
-	<!--Kommentaar-->
-	<label for="comment">comment</label><br>
-	<input name="comment">
+	<p><label for="category">Vali eriala:</label><br>
+	<select name="category" id="category" size="4" required>
+	<option value="BMKKI" disabled="" class="cat_col">Balti filmi, meedia, kunstide ja kommunikatsiooni instituut</option>
+	<option value="Ajakirjadnus">Ajakirjandus</option>
+	<option value="Kunst,Muusika,Multimeedia">Kunst,Muusika,Multimeedia</option>
+	<option value="Koreograafia">Koreograafia</option>
+	<option value="Audiovisuaalne meedia">Audiovisuaalne meedia</option>
+	<option value="Reklaam ja imagoloogia">Reklaam ja imagoloogia</option>
+	<option value="Ristmeedia filmis ja televisioonis">Ristmeedia filmis ja televisioonis</option>
+	<option value="Suhtekorraldus">Suhtekorraldus</option>
+	<option value="Reklaam ja imagoloogia">Reklaam ja imagoloogia</option>
+	<option value="" disabled="DI" class="cat_col">Digitehnoloogiate instituut</option>
+	<option value="Informaatika">Informaatika</option>
+	<option value="Infoteadus">Infoteadus</option>
+	<option value="Infoteadus">Matemaatika</option>
+	<option value="" disabled="HTI" class="cat_col">Haridusteaduste instituut</option>
+	<option value="Alushariduse pedagoog">Alushariduse pedagoog</option>
+	<option value="Andragoogika">Andragoogika</option>
+	<option value="Eripedagoogika">Eripedagoogika</option>
+	<option value="Klassiopetaja">Klassiopetaja</option>
+	<option value="Kutsepadagoogika">Kutsepadagoogika</option>
+	<option value="Noorsootoo">Noorsootoo</option>
+	<option value="Pedagoogika">Pedagoogika</option>
+
 	
+	</select>
+	<br><br>
+	<!--pealkiri-->
+	<label for="headline">Teema pealkiri</label><br>
+	<input name="headline">
+	<br>
+	<!--Kommentaar-->
+	<textarea rows="4" cols="50" name="comment">Comment...</textarea>
+	<br><br>
 	<input type="submit" value="postita">
 	
 </form>
@@ -61,6 +90,6 @@ Digitehnoloogiate instituut
 Haridusteaduste instituut
 Humanitaarteaduste instituut
 Loodus- ja terviseteaduste instituut
-‹hiskonnateaduste instituut
-Haapsalu Kolled˛
-Rakvere Kolled˛-->
+–¨hiskonnateaduste instituut
+Haapsalu Kolled—é
+Rakvere Kolled—é-->
