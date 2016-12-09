55c92560aad0ddc9f2fbf986cd290ae73f3d62a4
Alar Aasa <alaraasa@users.noreply.github.com>
2016-12-10 00:49:03 +0200

    Scraper works

--- scraper/scraper.php
+++ scraper/scraper.php
@@ -1,6 +1,8 @@
 <?php
+
 include "simple_html_dom.php";
 require_once ("functions.php");
+mb_internal_encoding("iso-8859-1");
 // Specialization
 if (isset($_GET["ryhm"])) {
     $ryhm = $_GET["ryhm"];
@@ -21,24 +23,80 @@ if (isset($_GET["grupp"])) {
 } else {
     $grupp = 1;
 }
-$data = [];
+
+$data = array();
+
 $url = 'http://www.tlu.ee/masio/index.php?id=ryhm&ryhm=' . $ryhm . '&time=' . $time . '#MASIO';
 $html = file_get_html($url);
 // GET DATES
 foreach ($html->find('div.dayname') as $dates){
+
+    //$spans = $dates->find("span");
+    //echo $spans[1];
     //remove HTML tags
     $dates = substr(strstr($dates, " "), 17);
     $dates = substr($dates,0 , -6);
-
+    
     $datesExplode = explode(' ', $dates);
-    array_shift($datesExplode);
-    // Rename full date name to number
+    array_shift($datesExplode); // Remove day name 
+    $dateData = monthToDate($datesExplode);
     $datesExplode[1] = monthToDate($datesExplode[1]);
-    $dates = implode("", $datesExplode);
-    array_push($data, $dates);
+    $datesFinished = implode($datesExplode); // Rename full month name to number
+}
+//->getAttribute('id')
+  //echo (count($html->find('div#mASIO')[0]->children()));
+foreach ($html->find('div#mASIO')[0]->children() as $div){
+    if($div->getAttribute('class') == "dayname"){
+        $div = strip_tags($div);
+        $dayExplode = explode(' ', $div);
+        array_shift($dayExplode);
+        $dayExplode[1] = monthToDate($dayExplode[1]);
+        $dayFinished = implode($dayExplode);
+        echo "////////" . $dayFinished . "\\\\\\\\\\\\\\\\<br>";
+    } else {
+      $spans = $div->find("span");
+      if(count($spans) > 0){
+        $time = $spans[0]->innertext;
+        $room = $spans[1]->innertext;
+        $subject = $spans[2]->innertext;
+    
+        //lesson code
+        $subject = explode(" ", $subject);
+        $lessonCode = $subject[0];
+        array_shift($subject);
+        $subject = implode($subject, " ");
+
+        $subject = explode("(", $subject);
+        $lessonName = $subject[0];
+        array_shift($subject);
+        $subject = implode($subject, " ");
+
+        $subject = explode(")", $subject);
+        if(strpos(utf8_encode($subject[0]), "rühm") !== false){
+            $group = filter_var($subject[0], FILTER_SANITIZE_NUMBER_INT);
+            $teacher = $subject[1];
+        } else {
+            $group = "ALL";
+            $teacher = $subject[0];
+        }
+
+        //$subject = array_shift($subject);
+        echo "Rühm ". $group . " | ";
+        echo "Kell " . $time . " | ";
+        echo $lessonCode . " | ";
+        echo $lessonName . " | ";
+        echo $teacher . " | ";
+        echo $room . "<br>";
+        
+        echo "<br>";
+    }
+
 }
 
-var_dump($data);
+}
+
+//clear_all($html);
+//var_dump($data["G1"]);
 
 
 
