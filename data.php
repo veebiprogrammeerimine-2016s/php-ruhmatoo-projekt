001e97cb9731280d24c154c6f15a120f10d0f1d6
karl107 <karl107@users.noreply.github.com>
2016-12-10 14:41:20 +0200

    navbar

--- data.php
+++ data.php
@@ -25,24 +25,66 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>SoundScape</title>
+	<title>Audify</title>
 	<meta name="description" content="SoundScape">
 	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-	
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+
 
 </head>
 
 <body>
+	<nav class="navbar navbar-inverse navbar-static-top">
+	  <div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">Audify</a>
+		</div>
 
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+			
+		  </ul>
+		  <form class="navbar-form navbar-left">
+			<div class="form-group">
+			  <input type="text" class="form-control" placeholder="Search">
+			</div>
+			<button type="submit" class="btn btn-default">Find</button>
+		  </form>
+		  <ul class="nav navbar-nav navbar-right">
+			
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$_SESSION["userEmail"];?><span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="user.php">User profile</a></li>
+				<li><a href="#">Settings</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="?logout=1">Sign out</a></li>
+			  </ul>
+			</li>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+	
+	<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
 	<header>
 		<div class="jumbotron">
 			<div class="container">
-				<h1>SoundScape</h1>
-				<h3>Quality is priority</h3><br>
+				<h1>Audify</h1>
 				<h4>Signed in as <a href="user.php"><?=$_SESSION["userEmail"];?></a></h4>
 				<h4><a href="?logout=1">Sign out</a></h4>
 			</div> 
--- login2.php
+++ login2.php
@@ -126,11 +126,11 @@
 	<title>SoundScape</title>
 	<meta name="description" content="SoundScape">
 	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-	
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+
 
 </head>
 
@@ -140,11 +140,42 @@
 
 <body>
 
+	<nav class="navbar navbar-inverse navbar-static-top">
+	  <div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">Audify</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+			
+		  </ul>
+		  <form class="navbar-form navbar-left">
+			<div class="form-group">
+			  <input type="text" class="form-control" placeholder="Search">
+			</div>
+			<button type="submit" class="btn btn-default">Find</button>
+		  </form>
+		  <ul class="nav navbar-nav navbar-right">
+			
+			
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
 	<header>
 		<div class="jumbotron">
 			<div class="container">
-				<h1>SoundScape</h1>
-				<h3>Quality is priority</h3>
+				<h1>Audify</h1>
 			</div> 
 		</div> 
 	</header>
@@ -222,7 +253,6 @@
 		
 	</div>
 	<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
