a98176f1cc1fc2110b174495b041e2f60dea9b52
tanelm <tanelmaasalu@gmail.com>
2016-12-14 09:31:45 +0200

    Add files via upload

---
+++ php-ruhmatoo-projekt/page/edit.php
@@ -0,0 +1,69 @@
+<?php
+	//edit.php
+	require("../functions.php");
+	
+	
+	//var_dump($_POST);
+	
+	//kas kasutaja uuendab andmeid
+	if(isset($_POST["update"])){
+		
+		$Animal->update($Helper->cleanInput($_POST["id"]), $Helper->cleanInput($_POST["type"]), $Helper->cleanInput($_POST["name"]), $Helper->cleanInput($_POST["age"]));
+		
+		//header("Location: animals.php?id=".$_POST["id"]."&success=true");
+        //exit();	
+		
+	}
+	
+	//kustutan
+	if(isset($_GET["delete"]) && isset($_GET["id"])){
+		
+		$Animal->delete($_GET["id"]);
+		
+		header("Location: animals.php");
+		exit();
+	}
+	
+	
+	
+	// kui ei ole id'd aadressireal siis suunan
+	if(!isset($_GET["id"])){
+		header("Location: animals.php");
+		exit();
+	}
+	
+	//saadan kaasa id
+	$c = $Animal->getSingle($_GET["id"]);
+	//var_dump($c);
+	//proov
+	if(isset($_GET["success"])){
+		echo "Salvestamine onnestus";
+	}
+
+	
+?>
+<?php require("../header.php"); ?>
+<br><br>
+<a href="animals.php"> Tagasi kuulutusele </a>
+
+<h2>Muuda kirjet</h2>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+	<input type="hidden" name="id" value="<?=$_GET["id"];?>" > 
+  	<label for="type" >Liik</label><br>
+	<input id="type" name="type" type="text" value="<?php echo $c->type;?>" ><br><br>
+  	<label for="name" >Nimi</label><br>
+	<input id="name" name="name" type="text" value="<?=$c->name;?>"><br><br>
+	<label for="age" >Vanus</label><br>
+	<input id="age" name="age" type="text" value="<?=$c->age;?>"><br><br>
+	
+	<input type="submit" name="update" value="Uuenda">
+  </form>
+  
+  
+ <br>
+ <br>
+ <br>
+ <a href="?id=<?=$_GET["id"];?>&delete=true">Kustuta</a>
+ <?php require("../footer.php"); ?>
+ 
+<body style='background-color:Silver'>
---
+++ php-ruhmatoo-projekt/page/insert_animal.php
@@ -0,0 +1,108 @@
+<?php
+   require("../functions.php");
+   
+   //kas on sisseloginud, kui ei ole siis
+   //suunata login lehele
+   if (!isset ($_SESSION["userId"])) {
+	   
+	   //header("Location: login.php");
+	   
+	}
+   
+   //kas ?loguout on aadressireal
+   if (isset($_GET["logout"])) {
+	   
+	   session_destroy();
+	   
+	   header("Location: login.php");
+	   exit();
+	   
+   }  
+   $informationError="";
+   
+   if ( isset($_POST["type"]) &&
+	     isset($_POST["name"]) &&
+		 isset($_POST["age"])&&
+		 isset($_POST["shelter"]) 	 &&
+		 !empty($_POST["type"]) &&
+		 !empty($_POST["name"]) &&
+		 !empty($_POST["age"])&&
+		 !empty($_POST["shelter"])		 ) {
+			
+		  
+		$type = cleanInput($_POST["type"]);
+        $name = cleanInput($_POST["name"]);
+        $age = cleanInput($_POST["age"]);
+		$shelter = cleanInput($_POST["shelter"]);
+		
+		$Animal->save($_POST["type"], $_POST["name"], $_POST["age"], $_POST["shelter"]);
+	}
+	
+	 if (empty($_POST["type"]) &&
+		empty($_POST["name"]) &&
+		empty($_POST["age"])&&
+		empty($_POST["shelter"]))
+{
+			 $informationError = "Täita tuleb kõik väljad!";
+			 
+		}
+		
+		
+		$shelter =$Shelter->getAll();
+?>
+
+<html>
+<body style='background-color:Silver'>
+    <head>
+	<?php require("../header.php"); ?>
+        <h1>Koduloomade registreerimine</h1>
+	</head>	
+        <body>
+            <h3>Andmed</h3>
+            <form method="POST">
+				<label>Liik</label><br>
+	            <select name="type" id="type" name="type">
+				<option value="Koer">Koer</option>
+				<option value="Kass">Kass</option>
+				<option value="Papagoi">Papagoi</option>
+				<option value="Janes">Janes</option>
+				<option value="Muu">Muu</option>
+				</select>
+	            <br><br>
+	
+	            <label>Looma nimi</label><br>
+	            <input name="name" type="text" >
+	            <br><br>
+	
+	            <label>Vanus</label><br>
+	            <input name="age" type="int" >
+	            <br><br>
+				
+				<label>Varjupaik</label><br>
+				<select name="shelter" type="text">
+				
+				<?php
+            
+				$listHtml = "";
+        	
+				foreach($shelter as $x){
+        		
+        		
+        		$listHtml .= "<option value='".$x->id."'>".$x->name."</option>";
+        
+				}
+        	
+				echo $listHtml;
+            
+        ?>
+    </select>
+	
+	
+	            <input type="submit" value="Salvesta">
+				<br><br>
+                </form>
+		</body>		
+</html>		
+
+<br><br>
+<?php require("../footer.php"); ?>
