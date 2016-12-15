1916abbd2dedac85031853cc7468f59af1f23e09
Gittan Kaus <GittanKaus@users.noreply.github.com>
2016-12-15 13:17:28 +0200

    Updated login.php

--- Page/login.php
+++ Page/login.php
@@ -19,11 +19,67 @@
 		
 		if (empty ($_POST["signupEmail"])) {
 			
-			$signupEmailError = "See väli on kohustuslik";
+			$signupEmailError = "Can't be empty!";
 			
 		} else {
 				
 			$signupEmail = $_POST["signupEmail"];
+		}
+		
+	}
+	
+	$signupPasswordError = "";
+	
+	if (isset ($_POST["signupPassword"])) {
+		
+		if (empty ($_POST["signupPassword"])) {
+			
+			$signupPasswordError = "Can't be empty!";
+			
+		} else {
+			
+			if (strlen ($_POST["signupPassword"]) < 9 ) {
+				
+				$signupPasswordError = "Password must be at least 9 characters";
+				
+			}
+			
+		}
+		
+	}
+	
+	if ( isset($_POST["signupEmail"]) &&
+		 isset($_POST["signupPassword"]) &&
+		 $signupEmailError == "" && 
+		 empty($signupPasswordError)
+	   ) {
+		echo "Saving...<br>";
+		echo "Email ".$signupEmail."<br>";
+		echo "Password ".$_POST["signupPassword"]."<br>"; 
+		
+		$password = hash("sha512", $_POST["signupPassword"]);
+		
+		echo "räsi ".$password."<br>";
+		
+		$User->signup($signupEmail, $password);
+		
+	}
+	
+	$notice = "";
+	
+	if (	isset($_POST["loginEmail"]) && 
+			isset($_POST["loginPassword"]) && 
+			!empty($_POST["loginEmail"]) && 
+			!empty($_POST["loginPassword"]) 
+	) {
+		$notice = $User->login($_POST["loginEmail"], $_POST["loginPassword"]);
+		
+		if(isset($notice->success)){
+			header("Location: login.php");
+			exit();
+		}else {
+			$notice = $notice->error;
+			var_dump($notice->error);
 		}
 		
 	}
