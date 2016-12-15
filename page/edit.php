2bdcfb1a9e18b62c23f119a0ecb79a7464ff2232
Melissa Bramanis <MelissaIngela@users.noreply.github.com>
2016-12-15 13:20:04 +0200

    edit page tööle2

--- page/edit.php
+++ page/edit.php
@@ -26,7 +26,68 @@
         exit();	
 		
 	}
+	function upload(){
 	
+	var_dump($_FILES);
+	if(isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"])){
+		$target_dir = "../pildid/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				echo "File is an image - " . $check["mime"] . ".";
+				$uploadOk = 1;
+			} else {
+				echo "File is not an image.";
+				$uploadOk = 0;
+			}
+		}
+		// Check if file already exists
+		if (file_exists($target_file)) {
+			echo "Sorry, file already exists.";
+			$uploadOk = 0;
+		}
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+			echo "Sorry, your file is too large.";
+			$uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+		&& $imageFileType != "gif" ) {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			$uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
+		} else {
+			
+			
+			$target_file = $target_dir.uniqid().".".$imageFileType;
+			
+			
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+				
+				// save file name to DB here
+				$a = new StdClass();
+				$a->name = $target_file;
+				return $a;
+				
+			} else {
+				return "Sorry, there was an error uploading your file.";
+			}
+		}
+	}else{
+		return "Please select the file that you want to upload!";
+	}
+	
+}
 	//saadan kaasa id
 	$c = $Note->getSingleNoteData($_GET["id"]);
 	//var_dump($c);
@@ -50,6 +111,11 @@
   	<label for="url" >url</label>
 	<br>
 	<img width='300' src="<?=$c->url;?>">
+	 Muuda pilti:
+    <input type="file" name="fileToUpload" id="Pilt">
+<br><br>
+	<input class="btn btn-success btn-sm hidden-xs" type="submit" name="submit" value="Postita">
+
 	<textarea  id="url" name="url"><?php echo $c->url;?></textarea><br>
   	
 	<input type="submit" name="update" value="Salvesta">
