a03c783e2c881f4009e09d0c6d0a3698da86f405
Andres Alla <privileeg@users.noreply.github.com>
2016-12-16 23:17:04 +0200

    upload working, file size limit though..

--- upload.php
+++ upload.php
@@ -26,46 +26,56 @@
 <?php
 
 
-	//UPLOAD
 	
-	
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-	$uploadOk = 1;
-	$audioFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-	
-	
-	// Check if file already exists
-	if (file_exists($target_file)) {
-		echo "Sorry, file already exists.";
-		$uploadOk = 0;
-	}
-	// Check file size
-	if ($_FILES["fileToUpload"]["size"] > 50000000) {
-		echo "Sorry, your file is too large.";
-		$uploadOk = 0;
-	}
-	// Allow certain file formats
-	if($audioFileType != "mp3" && $audioFileType != "wav") {
-		echo "Sorry, only MP3 and WAV files are allowed.";
-		$uploadOk = 0;
-	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-		echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-	} else {
-		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-			
-			upload($email, $fileToUpload);
-			
-		} else {
-			echo "Sorry, there was an error uploading your file.";
-		}
-	}
-	
-	
+if(isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"])){
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    /* Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    */
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 50000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "mp3" && $imageFileType != "wav") {
+        echo "Sorry, only MP3 and WAV files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            
+            // save file name to DB here
+            
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}else{
+    echo "Please select the file that you want to upload!";
+}
+
 	
 ?>
 
@@ -137,11 +147,12 @@
 				
 				<h1>Upload your track</h1>
 
-				<form action="upload.php" method="Post" enctype="multipart/form-data">
-					Select track to upload:
-					<input type="file" name="fileToUpload" id="fileToUpload">
-					<input type="submit" value="Upload Audio" name="submit">
-				</form>
+					<form action="upload.php" method="post" enctype="multipart/form-data">
+						Select audio to upload:
+						<input type="file" name="fileToUpload" id="fileToUpload">
+						<input type="submit" value="Upload Audio" name="submit">
+					</form>
+
 
 				
 				
