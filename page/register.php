3887212f65ee74722737bba8f33520dabc2c0838
Kristel Roosimaa <krisr12@users.noreply.github.com>
2016-12-22 14:51:19 +0200

    new login and half register form

---
+++ page/register.php
@@ -0,0 +1,15 @@
+<title>Registreeri!</title>
+<h1>Registreeri end meie saidile</h1>
+
+<link rel="stylesheet" type="text/css" href="../styles/newlogin.css">
+
+<div class="login-page">
+<div class="form">
+    <form class="form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>Loo</button>
+      <p class="message">Oled juba registreerunud? <a href="login.php">Logi sisse</a></p>
+    </form>
+</div>
