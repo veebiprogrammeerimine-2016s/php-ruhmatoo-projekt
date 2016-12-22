645a1ba2607cdd90ac2aaf300664a8eafd9eb591
Mihkel Põder <mpoder@users.noreply.github.com>
2016-12-22 15:22:44 +0200

    smaller changes

--- page/footer.php
+++ page/footer.php
@@ -2,12 +2,12 @@
 <div class="row footer" style="margin-top: 2em; background: gray;">
 <div class="c-4">
 <h5 style="margin-top: 0em; margin-bottom: .5em;"><?php echo $appName;?></h5>
-<p style="margin-top: 0; margin-bottom: 0;"><a href="help.php">Abi</a> | <a href="privacy.php">Privaatsus</a></p>
+<p style="margin-top: 0; margin-bottom: 0;"><a href="help.php">Abi</a> < <a href="privacy.php">Privaatsus</a></p>
 </div>
 <div class="c-4">
 <h5 style="margin-top: 0em;">Tagasiside</h5>
 <p style="margin-top: 0; margin-bottom: 0;">Tagasiside veebisaidi kohta saatke palun <a href="feedback.php">siin.</a></p>
-<p style="margin-top: 0; margin-bottom: 0;">Kaebusi kasutajate kohta on võimalik saata <a href="complaint.php">siin.</a></p>
+<p style="margin-top: 0; margin-bottom: 0;">Kaebusi kasutajate kohta on võimalik saata <a href="complaints.php">siin.</a></p>
 </div>
 <div class="c-4">
 <h5 style="margin-top: 0em;">Kontakt</h5>
