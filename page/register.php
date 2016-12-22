887d818cb5d8896c3ef4f92def29ad090f4d9982
Kristel Roosimaa <krisr12@users.noreply.github.com>
2016-12-22 16:01:35 +0200

    little fixes

--- page/register.php
+++ page/register.php
@@ -2,14 +2,15 @@
 
 <link rel="stylesheet" type="text/css" href="../styles/newlogin.css">
 
+
 <div class="login-page">
-<div class="form">
     <form class="form">
-      <input type="text" placeholder="Nimi"/>
+      <input type="text" placeholder="Eesnimi"/>
+	  <input type="text" placeholder="Perekonnanimi"/>
       <input type="password" placeholder="Parool"/>
       <input type="email" placeholder="E-maili aadress"/>
       <button>Loo kasutaja</button>
       <p class="message">Oled juba registreerunud? <a href="login.php">Logi sisse</a></p>
-	  <p class="message">Omad ametit mida soovid meie lehel pakkuda? <a href="">Registreeru töömehena!</a></p>
+	  <p class="message">Omad ametit mida soovid meie lehel pakkuda? <a href="">Registreeru töötajana!</a></p>
     </form>
 </div>
