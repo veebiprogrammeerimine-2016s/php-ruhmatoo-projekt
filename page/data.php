8dfb2f9e00446583752c55a3f4f53248e2bc49d2
Marko Kollo <mrxkollo@users.noreply.github.com>
2016-12-30 18:56:36 +0200

    Client-side valideerimine.

--- page/data.php
+++ page/data.php
@@ -25,6 +25,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Javascript files-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="../style_script/js/bootstrap.min.js"></script>
+    <script src="../style_script/js/jquery.cookie.js"></script>
+    <script src="../style_script/js/lightbox.min.js"></script>
+    <script src="../style_script/js/front.js"></script>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>UNILIFE</title>
@@ -48,6 +55,10 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+
+
+
 </head>
 <body>
 
@@ -204,12 +215,6 @@
         </div>
     </footer>
 
-    <!-- Javascript files-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></>
-    <script src="../style_script/js/bootstrap.min.js"></>
-    <script src="../style_script/js/jquery.cookie.js"> </>
-        <script src="../style_script/js/lightbox.min.js"></>
-            <script src="../style_script/js/front.js"></>
 
 </body>
 </html>
