5e1b0bc14706f6a9d279b2e862465a1bcb12c0b4
Kärol-Milaine Jürgenson <karolmilaine@users.noreply.github.com>
2016-12-31 13:27:53 +0200

    something

--- page/homework.php
+++ page/homework.php
@@ -24,45 +24,44 @@
 <html lang="en">
 <head>
 
-    <!-- Javascript files-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="../style_script/js/jquery.cookie.js"></script>
-    <script src="../style_script/js/bootstrap.min.js"></script>
-    <script src="../style_script/js/lightbox.min.js"></script>
-    <script src="../style_script/js/front.js"></script>
-    <script src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.js"></script>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>UNILIFE</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="../style_script/css/bootstrap.min.css">
-    <!-- Font Awesome and Pixeden Icon Stroke icon fonts-->
-    <link rel="stylesheet" href="../style_script/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../style_script/css/pe-icon-7-stroke.css">
-    <!-- Google fonts - Roboto-->
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,700">
-    <!-- lightbox-->
-    <link rel="stylesheet" href="../style_script/css/lightbox.min.css">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="../style_script/css/style.blue.css" id="theme-stylesheet">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="../style_script/img/favicon.png">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-
+    <!--  jQuery -->
+    <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
 
     <!-- Bootstrap Date-Picker Plugin -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
 
+   <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>UNILIFE</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="../style_script/css/bootstrap.min.css">
+
+    <!-- Font Awesome and Pixeden Icon Stroke icon fonts-->
+    <link rel="stylesheet" href="../style_script/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../style_script/css/pe-icon-7-stroke.css">
+
+    <!-- Google fonts - Roboto-->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,700">
+
+    <!-- lightbox-->
+    <link rel="stylesheet" href="../style_script/css/lightbox.min.css">
+
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="../style_script/css/style.blue.css" id="theme-stylesheet">
+
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="../style_script/img/favicon.png">
+
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 
 </head>
 
@@ -181,7 +180,7 @@
                 <div class="form-group">
                     <label class="col-md-4 control-label" for="date">Tähtaeg</label>
                     <div class="col-md-4">
-                        <input class="form-control" id="date" name="date" placeholder="DD/MM/YYY" type="text"/>
+                        <input class="form-control" name="date" placeholder="DD/MM/YYY" type="text"/>
                     </div>
                 </div>
 
@@ -194,6 +193,11 @@
                     </div>
                 </div>
 
+
+
+                <!-- Include Date Range Picker -->
+                    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
                     <script>
                         $(document).ready(function(){
@@ -215,21 +219,7 @@
             </fieldset>
         </form>
 
-        <script>
 
-            $("#homeworkform").validate({
-
-                debug: true,
-                rules: {
-                    homeworkDescription: {required: true, minlength: 5},
-                    date: {required: true, date: true}},
-
-                messages:{
-                    homeworkDescription: {required: "Palun sisesta mingi kirjeldus.", minlength: "Palun sisestage midagi sisukamat."},
-                    date: {required: "Palun sisestage tähtaeg."}}
-            });
-
-        </script>
 
         <form class="form-horizontal" method="post" name="readingform">
             <fieldset>
@@ -430,5 +420,12 @@
     </div>
 </footer>
 
-</body>
-</html>
+<!-- Javascript files-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></>
+<script src="../style_script/js/bootstrap.min.js"></>
+<script src="../style_script/js/jquery.cookie.js"> </>
+    <script src="../style_script/js/lightbox.min.js"></>
+        <script src="../style_script/js/front.js"></>
+        </body>
+        </html>
+
