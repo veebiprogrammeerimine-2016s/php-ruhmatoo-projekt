9380688bfff0011fd07911ec860cbb4410e68ed7
Kärol-Milaine Jürgenson <karolmilaine@users.noreply.github.com>
2017-01-02 15:34:33 +0200

    updated contact table, now it looks nice on phones too

--- page/teachers.php
+++ page/teachers.php
@@ -36,52 +36,75 @@ if(isset($_GET["logout"])){
 </section>
 
 
-<table align="center" class="table table-striped table-hover " color="black">
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset='UTF-8'>
 
-    <tr>
-        <th>Nimi</th>
-        <th>Amet</th>
-        <th>Aine</th>
-        <th>Kontaktid</th>
-        <th>Ruum</th>
-    </tr>
-    <tr>
-        <td>Romil Rõbtsenkov</td>
-        <td>Tarkvaratehnika õpetaja</td>
-        <td>Veebiprogrammeerimine</td>
-        <td>romil.robtsenkov@tlu.ee</td>
-        <td>A-430</td>
-    </tr>
-    <tr>
-        <td>Jaagup Kippar</td>
-        <td>Tarkvaratehnika lektor</td>
-        <td>Andmebaaside projekteerimine</td>
-        <td>jaagup.kippar@tlu.ee</td>
-        <td>A-426</td>
-    </tr>
-    <tr>
-        <td>Inga Petuhhov</td>
-        <td>Tarkvaratehnika õpetaja</td>
-        <td>Programmeerimise alused</td>
-        <td>inga.petuhhov@tlu.ee</td>
-        <td>A-427</td>
-    </tr>
-    <tr>
-        <td>Tanel Toova</td>
-        <td>Süsteemiadministraator</td>
-        <td>Operatsioonisüsteemide alused</td>
-        <td>tanel.toova@tlu.ee</td>
-        <td>A-432</td>
-    </tr>
-    <tr>
-        <td>Tatjana Tamberg</td>
-        <td>Matemaatika dotsent</td>
-        <td>Diskreetsed struktuurid</td>
-        <td>tatjana.tamberg@tlu.ee</td>
-        <td>A-441</td>
-    </tr>
-</table>
+        <title>Responsive Table</title>
 
-<br><br><br><br>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="../style_script/css/table.css">
+
+    </head>
+
+    <body>
+    <div id="page-wrap">
+
+        <table align="center" class="table table-striped table-hover " >
+            <thead>
+            <tr>
+                <th>Nimi</th>
+                <th>Amet</th>
+                <th>Aine</th>
+                <th>Kontaktid</th>
+                <th>Ruum</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Romil Rõbtsenkov</td>
+                <td>Tarkvaratehnika õpetaja</td>
+                <td>Veebiprogrammeerimine</td>
+                <td>romil.robtsenkov@tlu.ee</td>
+                <td>A-430</td>
+            </tr>
+            <tr>
+                <td>Jaagup Kippar</td>
+                <td>Tarkvaratehnika lektor</td>
+                <td>Andmebaaside projekteerimine</td>
+                <td>jaagup.kippar@tlu.ee</td>
+                <td>A-426</td>
+            </tr>
+            <tr>
+                <td>Inga Petuhhov</td>
+                <td>Tarkvaratehnika õpetaja</td>
+                <td>Programmeerimise alused</td>
+                <td>inga.petuhhov@tlu.ee</td>
+                <td>A-427</td>
+            </tr>
+            <tr>
+                <td>Tanel Toova</td>
+                <td>Süsteemiadministraator</td>
+                <td>Operatsioonisüsteemide alused</td>
+                <td>tanel.toova@tlu.ee</td>
+                <td>A-432</td>
+            </tr>
+            <tr>
+                <td>Tatjana Tamberg</td>
+                <td>Matemaatika dotsent</td>
+                <td>Diskreetsed struktuurid</td>
+                <td>tatjana.tamberg@tlu.ee</td>
+                <td>A-441</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <br><br>
+    </body>
+    </html>
+
 
 <?php require("footer.php");
