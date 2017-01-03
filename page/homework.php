ca6f2483e62f75b170b9f9d31b3c6a96a4009eaa
Marko Kollo <mrxkollo@users.noreply.github.com>
2017-01-03 18:46:16 +0200

    Õpetajate tabel

--- page/homework.php
+++ page/homework.php
@@ -85,29 +85,6 @@
     }
 
 
-    if(isset($_POST["sendTeacher"])){
-
-        if(
-            isset($_POST["teacher"]) &&
-            isset($_POST["roomnumber"]) &&
-            isset($_POST["email"]) &&
-            isset($_POST["material"]) &&
-            !empty($_POST["teacher"]) &&
-            !empty($_POST["roomnumber"]) &&
-            !empty($_POST["email"]) &&
-            !empty($_POST["material"])){
-
-                $Teacher->save(
-                    $Helper->cleanInput($_POST["teacher"]),
-                    $Helper->cleanInput($_POST["roomnumber"]),
-                    $Helper->cleanInput($_POST["email"]),
-                    $Helper->cleanInput($_POST["material"]),
-                    $Helper->cleanInput($_SESSION["userEmail"])
-                );
-                header("Location: homework.php");
-                exit();
-            }
-    }
 
 
 ?>
@@ -245,77 +222,6 @@
             </fieldset>
         </form>
 
-
-
-        <form class="form-horizontal" method="post" id="teacherform" name="teacherform">
-            <fieldset>
-
-                <!-- Form Name -->
-                <legend>Õpetajad</legend>
-
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="teacher">Nimi(*)</label>
-                    <div class="col-md-4">
-                        <input id="teacher" name="teacher" type="text" class="form-control input-md" >
-                    </div>
-                </div>
-
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="roomnumber">Ruuminumber(*)</label>
-                    <div class="col-md-4">
-                        <input id="roomnumber" name="roomnumber" placeholder="T302" type="text" class="form-control input-md" >
-
-                    </div>
-                </div>
-
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="email">Email(*)</label>
-                    <div class="col-md-4">
-                        <input id="email" name="email" type="text" placeholder="eesnimi.perekonnanimi@tlu.ee" class="form-control input-md" >
-
-                    </div>
-                </div>
-
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="material">Kodulehe aadress</label>
-                    <div class="col-md-4">
-                        <input id="material" name="material" value="" placeholder="http://www.mingisait.tlu.ee" type="text" class="form-control input-md">
-                    </div>
-                </div>
-
-                <!-- Button -->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for=""></label>
-                    <div class="col-md-4">
-                        <button class="btn btn-primary" name="sendTeacher" type="submit">Salvesta</button>
-                    </div>
-                </div>
-
-            </fieldset>
-        </form>
-
-        <script>
-
-            $("#teacherform").validate({
-
-                rules: {
-                    teacher: {required: true},
-                    roomnumber: {required: true},
-                    email: {required: true, email: true},
-                    material: {url: true}},
-
-                messages:{
-                    teacher: {required: "Palun sisestage õpetaja nimi."},
-                    bookauthor: {required: "Palun sisestage õppejõu ametiruum."},
-                    email: {required: "Palun sisestage õppejõu email", email: "Palun sisestage korrektne email"},
-                    material: {url: "Palun sisestage korrektne http link."}
-            }});
-
-        </script>
 
         <form class="form-horizontal" method="post" id="classform" name="classform">
             <fieldset>
--- page/teachers.php
+++ page/teachers.php
@@ -1,24 +1,54 @@
 <?php
 
 require("functions.php");
+require("../class/Teacher.class.php");
+$Teacher = new Teacher($mysqli);
+$allTeachers = $Teacher->get($_SESSION["userEmail"]);
 
-//Kui ei ole kasutaja ID
+
 
 if(!isset($_SESSION["userEmail"])){
-
-    //Suuna sisselogimis lehele
     header("Location: login.php");
     exit();
 }
 
-//Kui on log out aadressireal, siis login v'lja
 if(isset($_GET["logout"])){
-
     session_destroy();
     header("Location: login.php");
     exit();
 }
+
+if(isset($_POST["sendTeacher"])){
+
+    if(
+        isset($_POST["teacher"]) &&
+        isset($_POST["roomnumber"]) &&
+        isset($_POST["email"]) &&
+        isset($_POST["material"]) &&
+        !empty($_POST["teacher"]) &&
+        !empty($_POST["roomnumber"]) &&
+        !empty($_POST["email"]) &&
+        !empty($_POST["material"])){
+
+        $Teacher->save(
+            $Helper->cleanInput($_POST["teacher"]),
+            $Helper->cleanInput($_POST["roomnumber"]),
+            $Helper->cleanInput($_POST["material"]),
+            $Helper->cleanInput($_POST["email"]),
+            $Helper->cleanInput($_SESSION["userEmail"])
+        );
+        header("Location: teachers.php");
+        exit();
+    }
+}
+
+
+
+
 ?>
+
+
+
 
 <?php require("header.php"); ?>
 
@@ -30,8 +60,75 @@ if(isset($_GET["logout"])){
                 <li>Kontaktid</li>
             </ul>
         </div>
-        <h1 class="heading">Kontaktid</h1>
-        <p class="lead">Võibolla lisada mingi tekst</p>
+        <form class="form-horizontal" method="post" id="teacherform" name="teacherform">
+            <fieldset>
+
+                <!-- Form Name -->
+                <legend>Õpetajad</legend>
+
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="teacher">Nimi(*)</label>
+                    <div class="col-md-4">
+                        <input id="teacher" name="teacher" type="text" class="form-control input-md" >
+                    </div>
+                </div>
+
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="roomnumber">Ruuminumber(*)</label>
+                    <div class="col-md-4">
+                        <input id="roomnumber" name="roomnumber" placeholder="T302" type="text" class="form-control input-md" >
+
+                    </div>
+                </div>
+
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="email">Email(*)</label>
+                    <div class="col-md-4">
+                        <input id="email" name="email" type="text" placeholder="eesnimi.perekonnanimi@tlu.ee" class="form-control input-md" >
+
+                    </div>
+                </div>
+
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="material">Kodulehe aadress</label>
+                    <div class="col-md-4">
+                        <input id="material" name="material" value="" placeholder="http://www.mingisait.tlu.ee" type="text" class="form-control input-md">
+                    </div>
+                </div>
+
+                <!-- Button -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for=""></label>
+                    <div class="col-md-4">
+                        <button class="btn btn-primary" name="sendTeacher" type="submit">Salvesta</button>
+                    </div>
+                </div>
+
+            </fieldset>
+        </form>
+
+        <script>
+
+            $("#teacherform").validate({
+
+                rules: {
+                    teacher: {required: true},
+                    roomnumber: {required: true},
+                    email: {required: true, email: true},
+                    material: {url: true}},
+
+                messages:{
+                    teacher: {required: "Palun sisestage õpetaja nimi."},
+                    bookauthor: {required: "Palun sisestage õppejõu ametiruum."},
+                    email: {required: "Palun sisestage õppejõu email", email: "Palun sisestage korrektne email"},
+                    material: {url: "Palun sisestage korrektne http link."}
+                }});
+
+        </script>
     </div>
 </section>
 
@@ -52,54 +149,33 @@ if(isset($_GET["logout"])){
     <body>
     <div id="page-wrap">
 
-        <table align="center" class="table table-striped table-hover " >
-            <thead>
-            <tr>
-                <th>Nimi</th>
-                <th>Amet</th>
-                <th>Aine</th>
-                <th>Kontaktid</th>
-                <th>Ruum</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>Romil Rõbtsenkov</td>
-                <td>Tarkvaratehnika õpetaja</td>
-                <td>Veebiprogrammeerimine</td>
-                <td>romil.robtsenkov@tlu.ee</td>
-                <td>A-430</td>
-            </tr>
-            <tr>
-                <td>Jaagup Kippar</td>
-                <td>Tarkvaratehnika lektor</td>
-                <td>Andmebaaside projekteerimine</td>
-                <td>jaagup.kippar@tlu.ee</td>
-                <td>A-426</td>
-            </tr>
-            <tr>
-                <td>Inga Petuhhov</td>
-                <td>Tarkvaratehnika õpetaja</td>
-                <td>Programmeerimise alused</td>
-                <td>inga.petuhhov@tlu.ee</td>
-                <td>A-427</td>
-            </tr>
-            <tr>
-                <td>Tanel Toova</td>
-                <td>Süsteemiadministraator</td>
-                <td>Operatsioonisüsteemide alused</td>
-                <td>tanel.toova@tlu.ee</td>
-                <td>A-432</td>
-            </tr>
-            <tr>
-                <td>Tatjana Tamberg</td>
-                <td>Matemaatika dotsent</td>
-                <td>Diskreetsed struktuurid</td>
-                <td>tatjana.tamberg@tlu.ee</td>
-                <td>A-441</td>
-            </tr>
-            </tbody>
-        </table>
+        <?php
+            $html ="";
+            $html = "<table allign='center' class='table table striped-table-hover'>";
+            $html .= "<thead>";
+            $html .= "<tr>";
+            $html .= "<th>Nimi</th>";
+            $html .= "<th>Klassiruum</th>";
+            $html .= "<th>Email</th>";
+            $html .= "</tr>";
+            $html .= "</thead>";
+            $html .= "<tbody>";
+
+            foreach($allTeachers as $teacher){
+
+                $html .= "<tr>";
+                $html .= "<td>$teacher->name</td>";
+                $html .= "<td>$teacher->classroom</td>";
+                $html .= "<td>$teacher->email</td>";
+                $html .=  "</tr>";
+            }
+
+
+            $html .= "</tbody>";
+            $html .= "</table>";
+            echo $html;
+        ?>
+
     </div>
 
     <br><br>
