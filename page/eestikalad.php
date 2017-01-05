241fee66aa05d8ef61f2f5a2d1ad3cae00ea1112
Aaro Videvik <Aarovidevik@users.noreply.github.com>
2017-01-05 14:52:44 +0200

    eestikalad.php

--- page/eestikalad.php
+++ page/eestikalad.php
@@ -11,15 +11,16 @@
 	<body >
 	
 	
-<section id="algus">
+<section id="haug">
 	<h1>Eesti kalad</h1>
 
 <h2>Haug</h2>
 <p>
 
-<img src="http://www.kalapeedia.ee/u/kalad/haug.jpg" alt="Haug">
+<img id = "haugipilt" src="http://www.kalapeedia.ee/u/kalad/haug.jpg" alt="Haug">
 <br>
 <br>
+
 Haug ehk harilik haug ehk havi (Esox lucius) on hauglaste sugukonda haugi perekonda kuuluv röövkala.
 
 Haug elab Euraasia ja Põhja-Ameerika põhjaosa sisevetes. Ta on üks kõige laiemalt levinud mageveekala. Venemaal puudub ta vaid Amuuri jõgikonnas, kus elab amuuri haug. Ukrainas puudub haug ainult Krimmis.[1]
@@ -32,6 +33,43 @@ Haugi tavaline suurus on 50–100 cm, aga ta võib olla üle 150 cm pikk ja üle
 	
 	</section>
 
+<section id="särg">
+	
+
+<h2>Särg</h2>
+<p>
+
+<img id = "särjepilt"src="http://www.matsalu.ee/teejuht/content/wiki/thumb_view/00/01/188.jpg" alt="Särg">
+<br>
+<br>
+
+Särge on teistest kaladest lihtne eristada silma oranzi või punase vikerkesta järgi. Keha on külgedelt lamenenud ja kaetud suurte korrapärastes ridades asetsevate soomustega. Värvuselt on ta sinakas- või rohekasmust, küljed ja kõht on hõbevalged. Särg on tavaliselt 10…20 cm pikkune ja kaalub kuni 10...150 g.
+
+Särg on laialt levinud peaaegu kõigis Eesti järvedes. Ta elutseb järve kaldaäärsetes, mudastes ja rikka taimestikuga osades. Särg on rangelt päevase eluviisiga, kes ööseks peitub taimede vahele ja heidab unne, võttes sisse kaldasendi ning toetades pea ja rinnauimedega põhjale.
+
+Särg on meie kõige taimtoidulisem kalaliik, kes sööb mändvetikate ja vesikatkude võrseid ja taimset kõdu. Erandiks on suuremate veekogude särjed (meil Võrtsjärves), kes tarvitavad toiduks ka kalu. Särg ise langeb ohvriks peaaegu kõikidele röövkaladele, kajakatele ja teistele veelindudele.
+
+Kudemise ajaks kasvab särjele selga pulmarüü (nn. helmeskate). Selle moodustavad sarvainest köbrukesed kala kehal, mis muudavad ta karedaks. Särjed koevad kevadel - aprilli lõpul või mai algul. Koelmud asuvad kalda lähedal madalvees, kus põhi on kaetud tihedate tarnapuhmastega või muu taimestikuga. Särg ei hõiva omale individuaalset kudemisala, vaid hoiduvad ühisesse kudemisvööndisse. Kudemine toimub rabeldes ja kärarikka veepladina saatel, mis võib olla nii tugev, et osa marjateri satub veest välja taimede veepealsetele osadele. Vastsed kooruvad sõltuvalt veetemperatuurist 9…14 päeva pärast. Särje eluiga võib ulatuda kuni 17 aastani.  </p>
+	
+	</section>
+
+<section id="ahven">
+<h2>Ahven</h2>
+<p>
+
+<img id = "ahvenapilt" src="http://kalad.loodus.ee/pildid/2637_ahven1.jpg" alt="Ahven">
+<br>
+<br>
+
+Särge on teistest kaladest lihtne eristada silma oranzi või punase vikerkesta järgi. Keha on külgedelt lamenenud ja kaetud suurte korrapärastes ridades asetsevate soomustega. Värvuselt on ta sinakas- või rohekasmust, küljed ja kõht on hõbevalged. Särg on tavaliselt 10…20 cm pikkune ja kaalub kuni 10...150 g.
+
+Särg on laialt levinud peaaegu kõigis Eesti järvedes. Ta elutseb järve kaldaäärsetes, mudastes ja rikka taimestikuga osades. Särg on rangelt päevase eluviisiga, kes ööseks peitub taimede vahele ja heidab unne, võttes sisse kaldasendi ning toetades pea ja rinnauimedega põhjale.
+
+Särg on meie kõige taimtoidulisem kalaliik, kes sööb mändvetikate ja vesikatkude võrseid ja taimset kõdu. Erandiks on suuremate veekogude särjed (meil Võrtsjärves), kes tarvitavad toiduks ka kalu. Särg ise langeb ohvriks peaaegu kõikidele röövkaladele, kajakatele ja teistele veelindudele.
+
+Kudemise ajaks kasvab särjele selga pulmarüü (nn. helmeskate). Selle moodustavad sarvainest köbrukesed kala kehal, mis muudavad ta karedaks. Särjed koevad kevadel - aprilli lõpul või mai algul. Koelmud asuvad kalda lähedal madalvees, kus põhi on kaetud tihedate tarnapuhmastega või muu taimestikuga. Särg ei hõiva omale individuaalset kudemisala, vaid hoiduvad ühisesse kudemisvööndisse. Kudemine toimub rabeldes ja kärarikka veepladina saatel, mis võib olla nii tugev, et osa marjateri satub veest välja taimede veepealsetele osadele. Vastsed kooruvad sõltuvalt veetemperatuurist 9…14 päeva pärast. Särje eluiga võib ulatuda kuni 17 aastani.  </p>
+	
+	</section>
 	</body>
 </html>
 </div>
