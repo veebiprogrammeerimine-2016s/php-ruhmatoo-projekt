ee281f1ab87ecc4923ad26c9ddb03f807c4e350f
Mihkel Põder <mpoder@users.noreply.github.com>
2017-01-05 16:04:24 +0200

    added general functions

--- class/class_data.php
+++ class/class_data.php
@@ -12,11 +12,30 @@ class internal{
   }
 
   function getWorkers() {
-
+    $sql = "select id from users where type='worker'";
+    $result = $this->conn->query($sql);
+    if ($result->num_rows > 0) {
+      $workers = array();
+      while ($row = $result->fetch_assoc()) {
+        $workers[] = $row["id"]
+      }
+      return $workers
+    } else {
+      return false;
+    }
   }
 
   function searchWorkers($searchterm) {
 
+  }
+
+  function getDistricts() {
+    $sql = "select id, name from districts";
+    $result = $this->conn->query($sql);
+    $districts = array();
+    while ($row = $result->fetch_assoc()) {
+      $districts[$row["id"]] = $row["name"];
+    } else {return false;}
   }
 
 }
