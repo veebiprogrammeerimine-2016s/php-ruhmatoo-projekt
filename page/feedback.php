9755e902e03d1a37dfadce5eb046c7b3dfc775c1
Elle Elisa Ivantsikova <elleelisa@users.noreply.github.com>
2017-01-05 19:47:07 +0200

    Changed some text styles on feedback page

--- page/feedback.php
+++ page/feedback.php
@@ -39,9 +39,9 @@ if (!empty($_POST["title"]) && !empty($_POST["content"])) {
 <div class="c-6">
 <h2>Saada tagasisidet</h2>
 <form method="post">
-<input type="text" name="title" placeholder="Pealkiri" style="width: 100%;" required><br>
+<input type="text" name="title" placeholder="Pealkiri" style="width: 100%; color: black;" required><br>
 <textarea name="content" placeholder="Tagasiside" style="width: 100%; font-family: Roboto; font-size: 1em; padding: 4px;" rows=5 required></textarea><br><br>
-<input type="submit" value="Saada tagasisidet">
+<input type="submit" class="button" value="Saada tagasisidet">
 </form>
 <br><br>
 <?php
