89d92a27bb2528756a735b4828fd366cc5930c09
gvalgerist <gvalgerist@users.noreply.github.com>
2017-01-05 16:19:18 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
-jalus
+<!DOCTYPE html>
+<html lang="en"> 
+<br>
+<div class="container">
+	
+	<div class="row">
+	
+		<div class="col-sm-12 col-sm-offset-0">
+	
+			<nav class="navbar navbar-inverse"> 
+				
+				<p class="navbar-text navbar-left">Sneakermarket | Contact email: mail@mail.ee | Tel. nr 53537462 | Digitehnoloogiate instituut | Tallinna Ulikool</p>
+			
+			</nav>
+		</div>
+	</div>
+</div>
+
 </body>
 </html>
