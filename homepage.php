05e55b7ea1ef0e28805a1c97c1dfddf5da0fdce3
Xenia <XeniaBelor@users.noreply.github.com>
2017-01-08 03:13:42 +0200

    homepage changes

--- homepage.php
+++ homepage.php
@@ -31,16 +31,7 @@
 <html>
 <style>
 	
-	.cat_col {
-		color:A11F2F; 
-		font-weight: 
-		600;
-	}
-	
-	clock {
-		color:black;
-	}
-	
+
 	table, td, th {
 		border: 1px solid black;
 	}
@@ -53,94 +44,40 @@
 	th {
 		height: 50px;
 	}
-	
-	body {
-		background: url(image/background.png);
-	}
-
 </style>
+
 <body>
 
 	<h2>Tee uus postitus</h2
 	<form method="POST">
 	<!--KATEGOORIA-->
-	<p><label for="category">Vali eriala:</label><br>
-	<select name="category" id="category" size="4" required>
-	<option value="BMKKI" disabled="" class="cat_col">Balti filmi, meedia, kunstide ja kommunikatsiooni instituut</option>
-	<option value="Ajakirjadnus">Ajakirjandus</option>
-	<option value="Kunst,Muusika,Multimeedia">Kunst,Muusika,Multimeedia</option>
-	<option value="Koreograafia">Koreograafia</option>
-	<option value="Audiovisuaalne meedia">Audiovisuaalne meedia</option>
-	<option value="Reklaam ja imagoloogia">Reklaam ja imagoloogia</option>
-	<option value="Ristmeedia filmis ja televisioonis">Ristmeedia filmis ja televisioonis</option>
-	<option value="Suhtekorraldus">Suhtekorraldus</option>
-	<option value="Reklaam ja imagoloogia">Reklaam ja imagoloogia</option>
-	<option value="" disabled="DI" class="cat_col">Digitehnoloogiate instituut</option>
-	<option value="Informaatika">Informaatika</option>
-	<option value="Infoteadus">Infoteadus</option>
-	<option value="Infoteadus">Matemaatika</option>
-	<option value="" disabled="HTI" class="cat_col">Haridusteaduste instituut</option>
-	<option value="Alushariduse pedagoog">Alushariduse pedagoog</option>
-	<option value="Andragoogika">Andragoogika</option>
-	<option value="Eripedagoogika">Eripedagoogika</option>
-	<option value="Klassiopetaja">Klassiopetaja</option>
-	<option value="Kutsepadagoogika">Kutsepadagoogika</option>
-	<option value="Noorsootoo">Noorsootoo</option>
-	<option value="Pedagoogika">Pedagoogika</option>
-	<option value="" disabled="DI" class="cat_col">Huminaarteaduste instituut</option>
-	<option value="Aasia uuringud">Aasia uuringud </option>
-	<option value="Ajalugu">Ajalugu</option>
-	<option value="Antropoloogia">Antropoloogia</option>
-	<option value="Eesti filoloogia">Eesti filoloogia</option>
-	<option value="Euroopa nüüdiskeeled ja kultuurid">Euroopa nüüdiskeeled ja kultuurid</option>
-	<option value="Filosoofia">Filosoofia</option>
-	<option value="Interdistsiplinaarsed humanitaarteadused">Interdistsiplinaarsed humanitaarteadused</option>
-	<option value="Kultuuriteadus">Kultuuriteadus</option>
-	<option value="Vene filoloogia">Vene filoloogia</option>
-	<option value="" disabled="DI" class="cat_col">Loodus- ja terviseteaduste instituut</option>
-	<option value="Bioloogia (kõrvalerialaga)">Bioloogia (kõrvalerialaga)</option>
-	<option value="Integreeritud loodusteadused">Integreeritud loodusteadused</option>
-	<option value="Integreeritud tehnoloogiad ja käsitöö">Integreeritud tehnoloogiad ja käsitöö</option>
-	<option value="Kehakultuur">Kehakultuur</option>
-	<option value="Keskkonnakorraldus">Keskkonnakorraldus</option>
-	<option value="Psühholoogia (psühholoogia suund)">Psühholoogia (psühholoogia suund)</option>
-	<option value="Psühholoogia (inimeseõpetuse suund)">Psühholoogia (inimeseõpetuse suund)</option>
-	<option value="Reakreatsioonikorraldus">Reakreatsioonikorraldus</option>
-	<option value="" disabled="DI" class="cat_col">Ühiskonnateaduste instituut</option>
-	<option value="Haldus- ja ärikorraldus">Haldus- ja ärikorraldus</option>
-	<option value="Interdistsiplinaarsed sotsiaalteadused">Interdistsiplinaarsed sotsiaalteadused</option>
-	<option value="Poliitika ja valitsemine">Poliitika ja valitsemine</option>
-	<option value="Riigiteadused">Riigiteadused</option>
-	<option value="Sotsiaaltöö">Sotsiaaltöö</option>
-	<option value="Sotsioloogia">Sotsioloogia</option>
-	<option value="Õigusteadus">Õigusteadus</option>
-	<option value="Õigusteadus (inglise keeles)">Õigusteadus (inglise keeles)</option>
-	<option value="" disabled="DI" class="cat_col">Haapsalu Kolledž</option>
-	<option value="Käsitöötehnoloogiad ja disain">Käsitöötehnoloogiad ja disain</option>
-	<option value="Liiklusohutus">Liiklusohutus</option>
-	<option value="Rakendusinformaatika">Rakendusinformaatika</option>
-	<option value="Tervisejuht">Tervisejuht</option>
-	<option value="" disabled="DI" class="cat_col">Rakvere Kolledž</option>
-	<option value="Alushariduse pedagoog">Alushariduse pedagoog</option>
-	<option value="Sotsiaalpedagoogika">Sotsiaalpedagoogika</option>
-	
+	<label for="category">Vali eriala:</label></br>
+	<select name="category" id="category" required>
+	<option value="">Näita</option>
+	<option value="Balti filmi, meedia, kunstide ja kommunikatsiooni instituut">Balti filmi, meedia, kunstide ja kommunikatsiooni instituut</option>
+	<option value="Digitehnoloogiate instituut">Digitehnoloogiate instituut</option>
+	<option value="Haridusteaduste instituut">Haridusteaduste instituut</option>
+	<option value="Huminaarteaduste instituut">Huminaarteaduste instituut</option>
+	<option value="Loodus- ja terviseteaduste instituut">Loodus- ja terviseteaduste instituut</option>
+	<option value="Ühiskonnateaduste instituut">Ühiskonnateaduste instituut</option>
+	<option value="Haapsalu Kolledž">Haapsalu Kolledž</option>
+	<option value="Rakvere Kolledž">Rakvere Kolledž</option>
 	</select>
+	
 	<br><br>
 	<!--pealkiri-->
 	<label for="headline">Uue postitus:</label><br>
 	<input placeholder="Pealkiri" name="headline">
-	<br>
 	<!--Kommentaar-->
+	<br>
 	<textarea rows="4" cols="50" placeholder="Kirjuta milline probleem sul tekkis.." name="comment"></textarea>
 	
 	<br><br>
 	<input type="submit" value="postita">
+	</form>
 	
 	<br><br>
 	<a href="?logout=1">Logi välja</a>
-	
-	<br><br>
-</form>
 </body>		
 </html>
 <?php 
---
+++ style/pagestyle.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+	
+	<style>
+	
+	body {
+		width:60%;
+		margin: 0 auto;
+	}
+	div.container {
+		width: 100%;
+		border: 1px solid gray;
+	}
+
+	header {
+		padding: 1em;
+		color: white;
+		background-color: black;
+		clear: left;
+		text-align: center;
+	}
+
+	nav {
+		float: left;
+		max-width: 160px;
+		margin: 0;
+		padding: 1em;
+	}
+
+	nav ul {
+		list-style-type: none;
+		padding: 0;
+		border: 1px solid gray;
+		background-color: pink;
+	}
+	   
+	nav ul a {
+		text-decoration: none;
+	}
+
+	article {
+		margin-center: 50px;
+		padding: 1em;
+		overflow: hidden;
+	}
+	
+	footer {
+		padding: 1em;
+		color: white;
+		background-color: black;
+		clear: left;
+		text-align: center;
+	}
+
+</style>
+</head>
+<body>
+
+<div class="container">
+
+<header>
+   <h1>City Gallery</h1>
+</header>
+  
+	<nav>
+	</nav>
+
+	<article>
+		<h1>London</h1>
+		<p>London is the capital city of England. It is the most populous city in the  United Kingdom, 
+		with a metropolitan area of over 13 million inhabitants.</p>
+	</article>
+
+<footer>Copyright &copy; W3Schools.com</footer>
+
+</div>
+
+</body>
+</html>
