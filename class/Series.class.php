65e84c6a830dd011c94e94b3006dcf2187a85662
Anu Sadam <anuxyo@users.noreply.github.com>
2017-01-09 18:28:25 +0200

    moved functions to class folder

--- class/Series.class.php
+++ class/Series.class.php
@@ -11,4 +11,29 @@ class Series{
     }
 }
     /*FUNKTSIOONID*/
+function getSeriesData() {
+	$database = "if16_ege";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+
+		$query = "SELECT * FROM user_tv_db GROUP BY title";
+		$myData = $mysqli->query($query);
+
+		$length = $myData->num_rows;
+
+		for($i = 0; $i < $length; $i++) {
+			$myData->data_seek($i);
+			$myDataRow = $myData->fetch_assoc();
+			$tvTitles[$i] = $myDataRow['title'];
+			$tvDate[$i] = $myDataRow['date'];
+		}
+
+		$myData->close();
+
+	$mysqli->close();
+
+	for($i = 0; $i < $length; $i++) {
+		echo '<option value="' . $tvTitles[$i] . '">' . $tvTitles[$i] . '</option>';
+	}
+
+}
 ?>
