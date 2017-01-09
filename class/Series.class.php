4f54c4f629f07bf5c7f4d769efa4a26492fca6ed
sandraannete <sandraannete@users.noreply.github.com>
2017-01-10 01:00:09 +0200

    added two functions into series.class.php

--- class/Series.class.php
+++ class/Series.class.php
@@ -36,4 +36,58 @@ function getSeriesData() {
 	}
 
 }
+
+
+function saveSeries ($tv_show) {
+		
+		$database = "if16_ege";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		$stmt = $mysqli->prepare("INSERT INTO user_tv_shows (tv_show) VALUES (?)");
+		
+		//kirjutab ette kus täpselt viga on 
+		echo $mysqli->error;
+		
+		$stmt->bind_param("s", $tv_show);
+		
+		if($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+		 	echo "ERROR ".$stmt->error;
+		}
+		
+	}
+
+	function getAllSeries() {
+		
+		$database = "if16_ege";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		$stmt = $mysqli->prepare("SELECT id, userid, tv_show FROM user_tv_shows");
+		
+		$stmt->bind_result($id, $userid, $tv_show);
+		$stmt->execute();
+		
+		//tekitan massiivi
+		$result = array();
+		
+		//while tingimus-tee seda kuni on rida andmeid
+		//mis vastab select lausele
+		// while järgne sulu sisu määrab kaua korratakse
+		while($stmt->fetch()) {
+			
+			//tekitan objekti
+			$tvshow = new StdClass();
+			$tvshow->id = $id;
+			$tvshow->tv_show = $tv_show;
+			
+			//echo $plate."<br>";
+			//igakord massiivi lisan juurde numbrimärgi
+			array_push($result, $tvshow);
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $result;
+	}
 ?>
--- page/calendar.php
+++ page/calendar.php
@@ -11,11 +11,18 @@
 ?>
 
 <?php require("../header.php"); ?>
+<br>
+<h3>Save some of your TV shows here</h3>
+        <form method="POST">
+            <input name="seriesname" placeholder="Name of the series" type="text">
+
+        <input type="submit" value="Save">
+        <br><br><br>
 
 <link href="calendar.css" type="text/css" rel="stylesheet" />
 <br>
 <input type="button" value="My profile" onclick="location='myprofile'" />
-<br>
+<br><br>
 <a href="?logout=2"> Log out</a>
 
 <?php
