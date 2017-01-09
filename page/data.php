0e0696e5d9c4b7e0f22cb3e6efa9ac9db413b14e
sandraannete <sandraannete@users.noreply.github.com>
2017-01-09 15:14:28 +0200

    idk

--- page/data.php
+++ page/data.php
@@ -75,8 +75,8 @@ if(isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"])){
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-<<<<<<< HEAD
-            //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+<<<<<<<HEAD
+//echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 =======
             echo "Your profile picture has been uploaded.";
 >>>>>>> 82dc2f87536c4f1f11641518b2f4e680a0f159ca
