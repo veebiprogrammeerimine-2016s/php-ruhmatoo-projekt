813c9912a90abd76cdf85c9a3ad42131ee6a5312
Mihkel Põder <mpoder@users.noreply.github.com>
2017-01-10 00:38:29 +0200

    made SQL code more easily replacable

--- database.sql
+++ database.sql
@@ -1,16 +1,17 @@
+drop database if exists if16_derkun_shazza;
 create database if16_derkun_shazza;
 use if16_derkun_shazza;
 
 create table usertypes (
-    type varchar(6) not null primary key
+    type varchar(6) unique not null primary key
 );
 
 insert into usertypes values ("user"), ("admin"), ("banned");
 
 create table users (
-  id int not null auto_increment primary key,
+  id int not null unique auto_increment primary key,
   username varchar(128) not null,
-  email varchar(128),
+  email varchar(128) not null unique,
   password varchar(128),
   type varchar(6),
   date_created datetime,
@@ -19,17 +20,17 @@ create table users (
 );
 
 create table categories (
-  id int not null auto_increment primary key,
+  id int not null unique auto_increment primary key,
   category varchar(255) not null
 );
 
 create table tags (
-  id int not null auto_increment primary key,
+  id int not null unique auto_increment primary key,
   tagname varchar(60)
 );
 
 create table posts (
-  id int not null auto_increment primary key,
+  id int not null unique auto_increment primary key,
   title varchar(255),
   content text,
   author int,
@@ -39,7 +40,7 @@ create table posts (
 );
 
 create table comments (
-  id int not null auto_increment primary key,
+  id int not null unique auto_increment primary key,
   author int,
   comment text,
   post int,
