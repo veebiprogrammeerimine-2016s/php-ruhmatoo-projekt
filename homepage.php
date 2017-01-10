88cd875a01ab7c508adf8ccfe8c194d0affaf13b
andrkalj <andrkalj@greeny.cs.tlu.ee>
2017-01-10 16:01:11 +0200

    added posts and comments and homepage

--- homepage.php
+++ homepage.php
@@ -10,9 +10,9 @@
 
 <body>
 	<ul>
-		<li><a class="active" href="homepage.php">HOME</a></li>
-		<li><a class="active1" href="forumpage.php">FORUM</a></li>
-		<li><a class="active1" href="">MY ACCOUNT</a></li>
+		<li><a class="active1" href="homepage.php">HOME</a></li>
+		<li><a class="active" href="forumpage.php">FORUM</a></li>
+		<li><a class="active1" href="userpage.php">MY ACCOUNT</a></li>
 		<li><a class="active1" href="?logout=1">LOG OUT</a></li>
 	</ul>
 
--- viewmycomments.php
+++ viewmycomments.php
@@ -1,1 +1,42 @@
-...
+<?php
+	
+	require("functions.php");
+	require("style/style.php");
+	require("style/pagestyle.php");
+	$comments = profile_comments();
+?>
+
+<html>
+
+<body>
+	<ul>
+		<li><a class="active1" href="homepage.php">HOME</a></li>
+		<li><a class="active1" href="forumpage.php">FORUM</a></li>
+		<li><a class="active" href="">MY ACCOUNT</a></li>
+		<li><a class="active1" href="?logout=1">LOG OUT</a></li>
+	</ul>
+
+<div style="page">
+
+<p class="down"> MY COMMENTS </p>
+
+<?php 
+$html2 = "<table>";
+	
+	$html2 .= "<tr>";
+		$html2 .= "<th>postitus</th>";		
+	$html2 .= "</tr>";
+	
+	foreach ($comments as $p) {
+	$html2 .= "<tr>";
+		$html2 .= "<td>".$p->feedback."</a></td>";
+	$html2 .= "</tr>";
+	}
+$html2 .= "</table>";
+echo $html2
+?>
+
+
+</div>	
+</body>
+</html>
