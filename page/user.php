5368e0eeb53eceef9f0aa597662225a4165c59aa
Anna Gubskaja <annagub@users.noreply.github.com>
2017-01-11 09:52:35 +0200

    github test

--- page/user.php
+++ page/user.php
@@ -5,6 +5,9 @@
 	require("../class/Level.class.php");
 	$Level = new Level($mysqli);
 	
+	require("../class/Finish.class.php");
+	$Finish = new Finish($mysqli);
+	
 	require("../class/Helper.class.php");
 	$Helper = new Helper();
 	
@@ -51,6 +54,28 @@
     $levels = $Level->get();
     $userLevels = $Level->getUser();
 	
+	if(isset($_GET["q"])){
+		
+		// kui otsib, võtame otsisõna aadressirealt
+		$q = $_GET["q"];
+		
+	}else{
+		
+		// otsisõna tühi
+		$q = "";
+	}
+	
+	$sort = "id";
+	$order = "ASC";
+	
+	if(isset($_GET["sort"]) && isset($_GET["order"])) {
+		$sort = $_GET["sort"];
+		$order = $_GET["order"];
+	}
+	
+	//otsisõna fn sisse
+	$finishData = $Finish->get($q, $sort, $order);
+	
 ?>
 
 
@@ -72,43 +97,18 @@
 	</div>
 </div>
 
-<div id="myModal" class="modal fade">
-  <div class="modal-dialog">
-    <div class="modal-content">
-		<form id="Ideas" method="post" role="form">
-		  <div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-			<h4 class="modal-title">Add your idea</h4>
-		  </div>
-		  <div class="modal-body">
-				<label for="idea" class="col-md-0 control-label"> <h3>Idea name</h3></label>
-			<div class="row">
-				<div class="col-md-4">
-					<input type="text" class="form-control" name="idea" id="idea" placeholder="This idea name is..." required>
-				</div>
-			</div>
-				<label for="description" class="col-md-0 control-label"><h3>Idea description</h3></label>
-			<div class="row">
-				<div class="form-group">
-					<div class="col-md-12">
-						<textarea class="form-control" rows="5" id="description" name= "description" placeholder="This idea is about..." required></textarea>
-					</div>
-				</div>
-			</div>
-		  </div>	
-		  <div class="modal-footer">
-			<button type="submit" class = "btn btn-success btn-sm ">Complete</button>
-			<button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Cancel</button>
-		  </div>
-		</form>
-    </div>
-  </div>
-</div>
-
 <div class="container">
+<div class="row">
+	<img src="../img/idea.png" class="pull-right" width="200" height="250">
+</div>
+<?php 
+	
+	$html = "<table class='table table-striped'>";
+	
 
 
 
+?>
 <h2>Programming languages you work with:</h2>
 <?php
     
@@ -161,6 +161,88 @@
 	
 </form>
 
+<?php 
+
+	$html = "<table class='table table-striped'>";
+	
+
+	
+	$html .= "<tr>";
+	
+		$idOrder = "ASC";
+		$arrow = "&darr;";
+		if (isset($_GET["order"]) && $_GET["order"] == "ASC"){
+			$idOrder = "DESC";
+			$arrow = "&uarr;";
+		}
+	
+		$html .= "<th>
+					<a href='?q=".$q."&sort=id&order=".$idOrder."'>
+						id ".$arrow."
+					</a>
+				 </th>";
+				 
+				 
+		$levelOrder = "ASC";
+		$arrow = "&darr;";
+		if (isset($_GET["order"]) && $_GET["order"] == "ASC"){
+			$levelOrder = "DESC";
+			$arrow = "&uarr;";
+		}
+		$html .= "<th>
+					<a href='?q=".$q."&sort=idea&order=".$levelOrder."'>
+						idea
+					</a>
+				 </th>";
+		$html .= "<th>
+					<a href='?q=".$q."&sort=description&order=".$levelOrder."'>
+						description
+					</a>
+				 </th>";
+		$html .= "<th>
+					<a href='?q=".$q."&sort=user&order=".$levelOrder."'>
+						user
+					</a>
+				 </th>";
+	$html .= "</tr>";
+	
+	//iga liikme kohta massiivis
+	foreach($finishData as $f){
+		
+		//echo $c->plate."<br>";
+		if($f->user == $_SESSION["userEmail"]){
+			$html .= "<tr>";
+				$html .= "<td>".$f->id."</td>";
+				$html .= "<td>".$f->idea."</td>";
+				$html .= "<td>".$f->description."</td>";
+				$html .= "<td>".$f->user."</td>";
+				
+				$html .= "<td><a class='btn btn-default btn-sm' href='edit.php?id=".$f->id."'><span class='glyphicon glyphicon-pencil'></span> Edit</a></td>";
+				
+			$html .= "</tr>";
+		}
+	}
+	
+	$html .= "</table>";
+	
+	echo $html;
+	
+	
+	$listHtml = "<br><br>";
+	
+	foreach($finishData as $f){
+		
+		
+		$listHtml .= "<h1>".$f->idea."</h1>";
+		$listHtml .= "<p>idea = ".$f->description."</p>";
+	}
+	
+	//echo $listHtml;
+	
+	
+	
+
+?>
 
 
 
