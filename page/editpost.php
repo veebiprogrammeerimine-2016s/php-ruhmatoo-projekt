cbd9f8456804a759cd7d60399a063251dc6950ab
Martin Kasak <MartinKasak@users.noreply.github.com>
2017-01-24 23:09:03 +0200

    data heading, condition, price , description, delete

--- page/editpost.php
+++ page/editpost.php
@@ -19,7 +19,36 @@ require("../functions.php");
 		header("Location: data.php");
 		exit();
 	}
+	
+$currentId = $_GET["id"];
+$recentPostData = $Products->getRecentPostInfo($currentId);
+$recentPostHeading = $recentPostData->heading;
+$recentPostCondition = $recentPostData->condition;
+$recentPostPrice = $recentPostData->price;
+$recentPostDescription = $recentPostData->description;
 
+if(isset($_POST["description"]) && isset($_POST["price"]) && isset($_POST["heading"]) && isset($_POST["condition"]) &&
+	 !empty($_POST["description"]) && !empty($_POST["price"]) && !empty($_POST["heading"])  &&  !empty($_POST["condition"])) {
+		
+		$updateStatus = 4;
+		$Products->saveproduct($currentId, $Helper->cleanInput($_POST["heading"]), $Helper->cleanInput($_POST["condition"]), $Helper->cleanInput($_POST["description"]), $Helper->cleanInput($_POST["price"]), $updateStatus);
+		
+		$recentPostData = $Products->getRecentPostInfo($currentId);
+		$recentId = $recentPostData->id;
+		$Products->deletePreviousPostVersions($currentId, $recentId);
+		
+		header("Location: editpost.php?id=".$currentId);
+		exit();
+	}
+	
+	if(isset($_POST["delete"])) {
+		
+		$Products->deleteUnfinishedPost($currentId);
+		
+		header("Location: myposts.php");
+		exit();
+	}
+	
 
 
 
