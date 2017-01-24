5d5b494f601281a3ccf8c75fe354f2482ef2f1de
Martin Kasak <MartinKasak@users.noreply.github.com>
2017-01-24 22:48:59 +0200

    minu kuuluste tabel

--- page/myposts.php
+++ page/myposts.php
@@ -30,3 +30,44 @@ require("../header.php");
 	
 	</ul>
 	<br><br>
+	
+	<div class="panel panel-info">
+<?php
+
+$myPosts = $Products->getAllMyPosts($sort, $direction);
+$direction = "ascending";
+
+	if(isset($_GET["direction"])) {
+		if($_GET["direction"] == "ascending") {
+			$direction = "descending";
+		}
+	}
+	$html = "<table class='table table-hover table-bordered'>";
+			
+		$html .= "<thead>";
+			$html .= "<tr>";
+				$html .= "<th><a href='?sort=heading&direction=".$direction."'>Toote nimi</a></th>";
+				$html .= "<th><a href='?sort=price&direction=".$direction."'>Hind</a></th>";
+				$html .= "<th><a href='?sort=description&direction=".$direction."'>Lühikirjeldus</a></th>";
+				$html .= "<th style='width:250px'>Muuda</th>";
+			$html .= "</tr>";
+		$html .= "</thead>";
+		
+		foreach($myPosts as $mp) {
+			
+			$html .= "<tr>";
+				$html .= "<td><a href='post.php?id=".$mp->postid."'>".$mp->heading."</a></td>";
+				$html .= "<td>".$mp->price."</td>";
+				$html .= "<td>".$mp->description."</td>";
+				$html .= "<td><a href='editpost.php?id=".$mp->postid."'><span class='glyphicon glyphicon-cog'></span></a></td>";
+			$html .= "</tr>";
+		}
+	$html .= "</table>";
+
+	echo $html;
+
+?>
+	</div>
+</div>
+
+<?php require("../footer.php"); ?>
