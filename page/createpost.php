d0d8dc04204ca67d3a537f8b12be923882a6aaa1
Martin Kasak <MartinKasak@users.noreply.github.com>
2017-01-24 23:24:34 +0200

    id logout userid

---
+++ page/createpost.php
@@ -0,0 +1,30 @@
+<?php
+require("../functions.php");
+
+	if(!isset($_SESSION["userId"])) {
+		header("Location: login.php");
+		exit();
+	}
+	if(isset($_GET["logout"])) {
+		session_destroy();
+		header("Location: login.php");
+		exit();
+	}
+	if(!isset($_GET["id"])) {
+		header("Location: data.php");
+		exit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+?>
