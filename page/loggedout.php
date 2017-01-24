d306c662e7e82127ef908b07a3482f7c959c82c7
Martin Kasak <MartinKasak@users.noreply.github.com>
2017-01-24 22:55:43 +0200

    loggedout kõikide postituste nägemine

---
+++ page/loggedout.php
@@ -0,0 +1,50 @@
+<?php
+
+require("../functions.php");
+
+	if(isset($_SESSION["userId"])) {
+		header("Location: productselect.php");
+		exit();
+	}
+
+
+$allPosts = $Products->getAllPosts();
+
+
+?>
+
+<?php require("../header.php"); ?>
+
+
+<!--  Kõikide kasutajate postitused -->
+
+<div class="container">
+<?php
+
+
+	$html = "<div class='row'>";
+		
+	foreach($allPosts as $p) {
+		$html .= "<div class='col-md-3'>";
+			$html .= "<div class='thumbnail'>";
+				$html .= "<img src='uploads/".$p->name."'>";
+				$html .= "<div class='caption'>";
+					$html .= "<h3>".$p->heading."</h3>";
+					$html .= "<p>".$p->description."</p>";
+					$html .= "<p><a href='#' class='btn btn-primary' role='button'>Vaata</a></p>";
+				$html .= "</div>";
+			$html .= "</div>";
+		$html .= "</div>";
+	}
+	
+	$html .= "</div>";
+	
+	echo $html;
+
+
+
+?>
+</div>
+
+
+<?php require("../footer.php"); ?>
