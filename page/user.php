e2bfe1f40f8aac4fd4c3c79b416f6ce14143ac18
Martin Kasak <MartinKasak@users.noreply.github.com>
2017-01-24 21:27:47 +0200

    kasutja id, tervitus message

---
+++ page/user.php
@@ -0,0 +1,34 @@
+<?php 
+
+	require("../functions.php");
+	
+	if (!isset($_SESSION["userId"])){
+		header("Location: login.php");
+		exit();
+	}
+
+	if (isset($_GET["logout"])) {
+		session_destroy();
+		header("Location: login.php");
+		exit();
+	}
+	$msg = "";
+	if(isset($_SESSION["message"])){
+		$msg = $_SESSION["message"];
+		
+		unset($_SESSION["message"]);
+	}
+	if ( isset($_POST["interest"]) && 
+		!empty($_POST["interest"])
+	  ) {
+		$Interest->saveInterest($Helper->cleanInput($_POST["interest"]));
+	}
+	if ( isset($_POST["userInterest"]) && 
+		!empty($_POST["userInterest"])
+	  ) {
+		$Interest->saveUserInterest($Helper->cleanInput($_POST["userInterest"]));
+	}
+    $interests = $Interest->getAllInterests();
+	$userInterests = $Interest->getAllUserInterests();
+?>
+<?php require("../header.php"); ?>
