f8a2dc9c28b8a79cfd18051b8c83cc2e19034690
Martin Kasak <MartinKasak@users.noreply.github.com>
2017-01-24 21:37:37 +0200

    muutujuad

--- page/signup.php
+++ page/signup.php
@@ -1,5 +1,52 @@
 <?php
 
+	require("../../../config.php");
+	require("../functions.php");
+	if(isset($_SESSION["userId"])){
+		
+		header("Location: productselect.php");
+		exit();
+	}
+
+	$signupemail = "";
+	$signupEmailError= "";
+	$signupPasswordError= "";
+	$reenterpasswordError= "";
+
+	$signupFirstname = "";
+	$signupFirstnameError = "";
+	$signupLastname = "";
+	$signupLastnameError = "";
+	
+	$signupGender = "";
+	$signupGenderError = "";
+	
+	$signupUsername = "";
+	$signupUsernameError = "";
+	$signupBDay = "";
+	$signupBDayError = "";
+	$signupBMonth = "";
+	$signupBMonthError = "";
+	$signupBYear = "";
+	$signupBYearError = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
