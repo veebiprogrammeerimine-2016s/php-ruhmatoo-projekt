01b9c4d64e9c43017ee32c77e02c9fc3032cdc25
Martin Kasak <MartinKasak@users.noreply.github.com>
2017-01-24 21:14:04 +0200

    signUp functiooni lisamine

--- class/user.class.php
+++ class/user.class.php
@@ -6,7 +6,18 @@ class User {
 		function __construct($mysqli){
 			$this->connection=$mysqli;
 		}
-
+	function signUp ($email, $password, $fistname, $lastname, $gender, $username, $DoB) {
+		$stmt = $this->connection->prepare("INSERT INTO prod_users(firstname, lastname, dateofbirth, gender, username, email, password, userlevel) VALUES(?, ?, ?, ?, ?, ?, ?, 1)");
+		echo $this->connection->error;
+		$stmt->bind_param("sssssss", $fistname, $lastname, $DoB, $gender, $username, $email, $password);
+		if($stmt->execute()) {
+			echo "salvestamine onnestus";
+		} else {
+			echo "ERROR".$stmt->error;
+		}
+		$stmt->close();
+		$this->connection->close();
+	}
 
 
 
